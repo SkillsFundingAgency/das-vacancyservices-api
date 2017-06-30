b3440ecb634983529658c735e7e216d04a2d37d4
Deven Shah <deven.shah@valtech.com>
2017-06-30 10:31:22 +0100

    - Added more fields to Vacancy.  - Added VacancyType enum

--- src/Esfa.Vacancy.Api.Types/Vacancy.cs
+++ src/Esfa.Vacancy.Api.Types/Vacancy.cs
@@ -16,5 +16,62 @@ namespace Esfa.Vacancy.Api.Types
         /// The Url to find the vacancy on this service
         /// </summary>
         public string Url { get; set; }
+
+        /// <summary>
+        /// Vacancy Title
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Short Description of the vacancy
+        /// </summary>
+        public string ShortDescription { get; set; }
+
+        /// <summary>
+        /// Long description of the vacancy
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Vacancy type
+        /// </summary>
+        public VacancyType VacancyType { get; set; }
+
+        /// <summary>
+        /// Weekly wage
+        /// </summary>
+        public decimal WeeklyWage { get; set; }
+
+        /// <summary>
+        /// Working week
+        /// </summary>
+        public string WorkingWeek { get; set; }
+
+        /// <summary>
+        /// Hour per week
+        /// </summary>
+        public decimal HoursPerWeek { get; set; }
+
+        /// <summary>
+        /// Duration of the vacancy
+        /// </summary>
+        public int Duration { get; set; }
+
+        /// <summary>
+        /// Expected start date
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// The date when vacancy was posted
+        /// </summary>
+        public DateTime DatePosted { get; set; }
+
+        /// <summary>
+        /// Number of positions available
+        /// </summary>
+        public int NumberofPositions { get; set; }
+
+
     }
 }
---
+++ src/Esfa.Vacancy.Api.Types/VacancyType.cs
@@ -0,0 +1,8 @@
+﻿namespace Esfa.Vacancy.Api.Types
+{
+    public enum VacancyType
+    {
+        Apprenticeship = 1,
+        Traineeship = 2
+    }
+}
