523202eb01a9e41fef11956b03a75f6efde66321
Deven Shah <deven.shah@valtech.com>
2017-06-30 13:30:06 +0100

    Committing missed out file

--- src/Esfa.Vacancy.Register.Application/Queries/GetVacancy/GetVacancyQueryHandler.cs
+++ src/Esfa.Vacancy.Register.Application/Queries/GetVacancy/GetVacancyQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Esfa.Vacancy.Register.Infrastructure.Repositories;
+using FluentValidation;
 using MediatR;
 using System.Threading.Tasks;
 
@@ -7,26 +8,22 @@ namespace Esfa.Vacancy.Register.Application.Queries.GetVacancy
     public class GetVacancyQueryHandler : IAsyncRequestHandler<GetVacancyRequest, GetVacancyResponse>
     {
         private readonly AbstractValidator<GetVacancyRequest> _validator;
+        private readonly IVacancyRepository _vacancyRepository;
 
-        public GetVacancyQueryHandler(AbstractValidator<GetVacancyRequest> validator)
+        public GetVacancyQueryHandler(AbstractValidator<GetVacancyRequest> validator,
+            IVacancyRepository vacancyRepository)
         {
             _validator = validator;
+            _vacancyRepository = vacancyRepository;
         }
 
-        public GetVacancyResponse Handle(GetVacancyRequest message)
+        public async Task<GetVacancyResponse> Handle(GetVacancyRequest message)
         {
             var validationResult = _validator.Validate(message);
 
             if (!validationResult.IsValid)
                 throw new ValidationException(validationResult.Errors);
 
-            //todo: add call to repo to retrieve from DB
-            _vacancyRepository = vacancyRepository;
-        }
-
-            var vacancy = new Domain.Entities.Vacancy {Reference = message.Reference};
-        public async Task<GetVacancyResponse> Handle(GetVacancyRequest message)
-        {
             var vacancy = await _vacancyRepository.GetVacancyByReferenceNumberAsync(message.Reference);
 
             return new GetVacancyResponse { Vacancy = vacancy };
