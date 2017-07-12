d6e2fcb794f451ae3f5cbb9dd9f035739477e05e
Mark Gwilliam <github@markgwilliam.com>
2017-07-12 07:38:29 +0100

    removed request logging

--- src/Esfa.Vacancy.Register.Api/Global.asax.cs
+++ src/Esfa.Vacancy.Register.Api/Global.asax.cs
@@ -36,11 +36,6 @@ namespace Esfa.Vacancy.Register.Api
         {
             var application = sender as HttpApplication;
             application?.Context?.Response.Headers.Remove("Server");
-
-            _logger = DependencyResolver.Current.GetService<ILog>();
-
-            var context = base.Context;
-            _logger.Info($"{context.Request.HttpMethod} {context.Request.Url.PathAndQuery}");
         }
 
         protected void Application_Error(object sender, EventArgs e)
