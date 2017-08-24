2ca3803c20b7fbbac7c242077ad8a0b87c937929
Chris Jensen <chrisjensenuk@gmail.com>
2017-08-24 11:34:26 +0100

    RA-1175 - Added AutoMapper configuration test

---
+++ src/Esfa.Vacancy.Register.UnitTests/Api/AutoMapperConfigTest.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Esfa.Vacancy.Register.Api.App_Start;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.Register.UnitTests.Api
+{
+    [TestFixture]
+    public class AutoMapperConfigTest
+    {
+        [Test]
+        public void ShouldHaveValidAutoMapperConfig()
+        {
+            AutoMapperConfig.Configure();
+
+            Mapper.Configuration.AssertConfigurationIsValid();
+        }
+        
+    }
+}
