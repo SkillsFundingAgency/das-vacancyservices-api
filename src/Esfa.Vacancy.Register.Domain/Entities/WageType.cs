2eaf399a3ab2d393b2e8ae9f623389bd9fd6c57d
sachin.patel <sachin.patel@valtech.co.uk>
2017-08-30 15:58:57 +0100

    RA-1174 - Introducing WageType (copied from Apprenticeships codebase) for context. Real Reference data.

---
+++ src/Esfa.Vacancy.Register.Domain/Entities/WageType.cs
@@ -0,0 +1,16 @@
+﻿namespace Esfa.Vacancy.Register.Domain.Entities
+{
+    public enum WageType
+    {
+        // NOTE: enum starts at zero to support direct mapping to legacy system.
+        LegacyText = 0,
+        LegacyWeekly = 1,
+        ApprenticeshipMinimum = 2,
+        NationalMinimum = 3,
+        Custom = 4,
+        CustomRange = 5,
+        CompetitiveSalary = 6,
+        ToBeAgreedUponAppointment = 7,
+        Unwaged = 8
+    }
+}
