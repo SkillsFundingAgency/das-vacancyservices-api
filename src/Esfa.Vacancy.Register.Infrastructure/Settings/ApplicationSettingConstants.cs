2a1a840537125d3b1b06d3238eb3bb607cf0eba9
Deven Shah <deven.shah@valtech.co.uk>
2017-09-25 15:06:19 +0100

    RA-1199 Added config settings required for Elastic Search

--- src/Esfa.Vacancy.Register.Infrastructure/Settings/ApplicationSettingConstants.cs
+++ src/Esfa.Vacancy.Register.Infrastructure/Settings/ApplicationSettingConstants.cs
@@ -5,5 +5,7 @@
         public const string AvmsPlusDatabaseConnectionStringKey = "AvmsPlusDatabaseConnectionString";
         public const string DasApprenticeshipInfoApiBaseUrl = "ApprenticeshipInfoApiBaseUrl";
         public const string LiveVacancyBaseUrl = "LiveVacancyBaseUrl";
+        public const string VacancySearchConnectionString = "VacancySearchConnectionString";
+        public const string ApprenticeshipIndexAlias = "ApprenticeshipIndexAlias";
     }
 }
