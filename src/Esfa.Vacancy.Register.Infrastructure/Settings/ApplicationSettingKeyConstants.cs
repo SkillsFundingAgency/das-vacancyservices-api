39dc0bc6b536e28eef4e00adb1a725088c3477c4
Deven Shah <deven.shah@valtech.co.uk>
2017-10-03 13:37:40 +0100

    RA-1199 Applied "Key" suffix to ApplicationSettingConstants

---
+++ src/Esfa.Vacancy.Register.Infrastructure/Settings/ApplicationSettingKeyConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Esfa.Vacancy.Register.Infrastructure.Settings
+{
+    public static class ApplicationSettingKeyConstants
+    {
+        public const string AvmsPlusDatabaseConnectionStringKey = "AvmsPlusDatabaseConnectionString";
+        public const string DasApprenticeshipInfoApiBaseUrlKey = "ApprenticeshipInfoApiBaseUrl";
+        public const string LiveVacancyBaseUrlKey = "LiveVacancyBaseUrl";
+        public const string VacancySearchUrlKey = "VacancySearchUrl";
+        public const string ApprenticeshipIndexAliasKey = "ApprenticeshipIndexAlias";
+    }
+}
