0ee5c41ad3e36e26fde9479507db7c73c8b7736b
Scott Wakefield <scott@expertcc.co.uk>
2017-10-03 17:03:29 +0100

    RA-1232 added initial tests for search orchestrator.

---
+++ src/Esfa.Vacancy.Register.UnitTests/SearchApprenticeship/Api/Orchestrators/GivenAnApprenticeshipSearchOrchestrator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Esfa.Vacancy.Register.Api.Orchestrators;
+using FluentAssertions;
+using FluentValidation;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.Register.UnitTests.SearchApprenticeship.Api.Orchestrators
+{
+    [TestFixture]
+    public class GivenAnApprenticeshipSearchOrchestrator
+    {
+        private ApprenticeshipSearchOrchestrator _orchestrator;
+
+        [SetUp]
+        public void WhenCallingSearchApprenticeship()
+        {
+            var mockMediator = new Mock<IMediator>();
+            _orchestrator = new ApprenticeshipSearchOrchestrator(mockMediator.Object);
+        }
+
+        [Test]
+        public void AndParametersAreNull_ThenThrowsValidationException()
+        {
+            Func<Task> action = async () => { await _orchestrator.SearchApprenticeship(null); };
+
+            action.ShouldThrow<ValidationException>().WithMessage("At least one search parameter is required.");
+        }
+    }
+}
