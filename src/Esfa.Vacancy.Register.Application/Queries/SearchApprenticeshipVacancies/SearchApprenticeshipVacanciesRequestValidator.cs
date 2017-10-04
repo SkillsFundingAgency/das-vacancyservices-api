7e8f385e98fafdb06dab27bd44ed640c03cc9164
Deven Shah <deven.shah@valtech.co.uk>
2017-10-04 12:52:06 +0100

    RA-1202 Added fields to request objects and respective validation and tests.

--- src/Esfa.Vacancy.Register.Application/Queries/SearchApprenticeshipVacancies/SearchApprenticeshipVacanciesRequestValidator.cs
+++ src/Esfa.Vacancy.Register.Application/Queries/SearchApprenticeshipVacancies/SearchApprenticeshipVacanciesRequestValidator.cs
@@ -13,6 +13,15 @@ namespace Esfa.Vacancy.Register.Application.Queries.SearchApprenticeshipVacancie
             RuleForEach(r => r.StandardCodes)
                 .Must(BeValidNumber)
                 .WithMessage((c, t) => $"{t} is invalid, expected a number.");
+
+            RuleFor(r => r.PageSize)
+                .GreaterThanOrEqualTo(1)
+                .WithMessage("Page size should be greater than or equal to 1")
+                .LessThanOrEqualTo(250)
+                .WithMessage("Page size should be less than or equal to 250");
+
+            RuleFor(r => r.PageNumber)
+                .GreaterThanOrEqualTo(1);
         }
 
         private bool BeValidNumber(string value)
---
+++ src/Esfa.Vacancy.Register.UnitTests/SearchApprenticeship/Application/GivenRequestValidator/WhenValidatingPageNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Esfa.Vacancy.Register.Application.Queries.SearchApprenticeshipVacancies;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.Register.UnitTests.SearchApprenticeship.Application.GivenRequestValidator
+{
+    public class WhenValidatingPageNumber
+    {
+        private SearchApprenticeshipVacanciesRequestValidator _validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _validator = new SearchApprenticeshipVacanciesRequestValidator();
+        }
+
+        [TestCase(0, false, "Anything less than one is invalid")]
+        [TestCase(1, true, "Minimum of one")]
+        [TestCase(22, true, "Anything more than one is valid")]
+        public void ThenValidate(int pageNumber, bool isValid, string reason)
+        {
+            var request = new SearchApprenticeshipVacanciesRequest { StandardCodes = new[] { "1" }, PageNumber = pageNumber };
+
+            var result = _validator.Validate(request);
+
+            result.IsValid.Should().Be(isValid, reason);
+        }
+    }
+}
