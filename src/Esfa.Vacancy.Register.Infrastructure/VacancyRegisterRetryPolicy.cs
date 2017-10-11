f99d64aa56b84caa8c0c256d0c36a72e838b7d7e
Chris Jensen <chrisjensenuk@gmail.com>
2017-10-11 16:04:41 +0100

    RA-1248 Added Polly and Test to ensure correct configuration

---
+++ src/Esfa.Vacancy.Register.Infrastructure/VacancyRegisterRetryPolicy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Polly;
+
+namespace Esfa.Vacancy.Register.Infrastructure
+{
+    public static class VacancyRegisterRetryPolicy
+    {
+        public static Policy GetFixedInterval(Action<Exception, TimeSpan, int, Context> onRetry)
+        {
+            return Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(3, (attempt) => TimeSpan.FromMilliseconds(500),
+                onRetry);
+        }
+    }
+}
