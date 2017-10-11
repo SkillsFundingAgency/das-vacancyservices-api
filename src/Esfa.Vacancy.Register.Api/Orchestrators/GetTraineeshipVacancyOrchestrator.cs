9685feb7e91e4d009471cef9077d08b837958ea1
sachin.patel <sachin.patel@valtech.co.uk>
2017-10-11 15:23:43 +0100

    RA-1244 - :construction: Using response mapper class to carry out mapping for response objects without the use of Automapper.

--- src/Esfa.Vacancy.Register.Api/Orchestrators/GetTraineeshipVacancyOrchestrator.cs
+++ src/Esfa.Vacancy.Register.Api/Orchestrators/GetTraineeshipVacancyOrchestrator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Esfa.Vacancy.Api.Types;
+using Esfa.Vacancy.Register.Api.Mappings;
 using Esfa.Vacancy.Register.Application.Queries.GetVacancy;
 using Esfa.Vacancy.Register.Infrastructure.Settings;
 using MediatR;
@@ -10,6 +11,7 @@ namespace Esfa.Vacancy.Register.Api.Orchestrators
     {
         private readonly IMediator _mediator;
         private readonly IProvideSettings _provideSettings;
+        private readonly VacancyResponseMapper _mapper = new VacancyResponseMapper();
 
         public GetTraineeshipVacancyOrchestrator(IMediator mediator, IProvideSettings provideSettings)
         {
@@ -21,60 +23,9 @@ namespace Esfa.Vacancy.Register.Api.Orchestrators
         {
             var liveVacancyBaseUrl = _provideSettings.GetSetting(ApplicationSettingKeyConstants.LiveVacancyBaseUrlKey);
             var response = await _mediator.Send(new GetTraineeshipVacancyRequest() { Reference = id });
-            var vacancy = MapToTraineeship(response.Vacancy);
+            var vacancy = _mapper.MapToTraineeshipVacancy(response.Vacancy);
             vacancy.VacancyUrl = $"{liveVacancyBaseUrl}/{vacancy.VacancyReference}";
             return vacancy;
-        }
-
-        private TraineeshipVacancy MapToTraineeship(Domain.Entities.Vacancy vacancy)
-        {
-            return new TraineeshipVacancy
-            {
-                VacancyReference = vacancy.VacancyReferenceNumber,
-                Title = vacancy.Title,
-                ShortDescription = vacancy.ShortDescription,
-                Description = vacancy.Description,
-                WorkingWeek = vacancy.WorkingWeek,
-                ExpectedDuration = vacancy.ExpectedDuration,
-                ExpectedStartDate = vacancy.ExpectedStartDate,
-                PostedDate = vacancy.PostedDate,
-                ApplicationClosingDate = vacancy.ApplicationClosingDate,
-                InterviewFromDate = vacancy.InterviewFromDate,
-                NumberOfPositions = vacancy.NumberOfPositions,
-                EmployerName = vacancy.EmployerName,
-                EmployerDescription = vacancy.EmployerDescription,
-                EmployerWebsite = vacancy.EmployerWebsite,
-                TrainingToBeProvided = vacancy.TrainingToBeProvided,
-                QualificationsRequired = vacancy.QualificationsRequired,
-                SkillsRequired = vacancy.SkillsRequired,
-                PersonalQualities = vacancy.PersonalQualities,
-                ImportantInformation = vacancy.ImportantInformation,
-                FutureProspects = vacancy.FutureProspects,
-                ThingsToConsider = vacancy.ThingsToConsider,
-                LocationType = (VacancyLocationType) vacancy.VacancyLocationTypeId,
-                SupplementaryQuestion1 = vacancy.SupplementaryQuestion1,
-                SupplementaryQuestion2 = vacancy.SupplementaryQuestion2,
-                Location = MapToLocation(vacancy.Location),
-                ContactName = vacancy.ContactName,
-                ContactEmail = vacancy.ContactEmail,
-                ContactNumber = vacancy.ContactNumber
-            };
-        }
-
-        private Address MapToLocation(Domain.Entities.Address location)
-        {
-            return new Address
-            {
-                AddressLine1 = location.AddressLine1,
-                AddressLine2 = location.AddressLine2,
-                AddressLine3 = location.AddressLine3,
-                AddressLine4 = location.AddressLine4,
-                AddressLine5 = location.AddressLine5,
-                Latitude = location.Latitude,
-                Longitude = location.Longitude,
-                PostCode = location.PostCode,
-                Town = location.Town
-            };
         }
     }
 }
