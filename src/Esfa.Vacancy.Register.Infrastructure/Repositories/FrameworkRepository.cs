f54948f9efb60e9a5e39d214e500aa6ae9fc9f0e
Scott Wakefield <scott@expertcc.co.uk>
2017-10-11 13:46:47 +0100

    RA-1232 new repository, only used by framework validator currently.

---
+++ src/Esfa.Vacancy.Register.Infrastructure/Repositories/FrameworkRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Esfa.Vacancy.Register.Domain.Entities;
+using Esfa.Vacancy.Register.Domain.Repositories;
+using Esfa.Vacancy.Register.Infrastructure.Settings;
+
+namespace Esfa.Vacancy.Register.Infrastructure.Repositories
+{
+    public class FrameworkRepository : IFrameworkRepository
+    {
+        private readonly IProvideSettings _settings;
+
+        private const string GetFrameworksQuery = @"
+            SELECT 
+                CodeName as Code,
+                FullName as Title
+            FROM 
+                dbo.ApprenticeshipFramework";
+
+        public FrameworkRepository(IProvideSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public async Task<IEnumerable<Framework>> GetFrameworksAsync()
+        {
+            var connectionString =
+                _settings.GetSetting(ApplicationSettingKeyConstants.AvmsPlusDatabaseConnectionStringKey);
+
+            using (var sqlConn = new SqlConnection(connectionString))
+            {
+                await sqlConn.OpenAsync();
+
+                var results =
+                    await sqlConn.QueryAsync<Framework>(GetFrameworksQuery);
+
+                return results;
+            }
+        }
+    }
+}
