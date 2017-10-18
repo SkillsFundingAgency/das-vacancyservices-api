008de99861ff3f36cacd643637e8fd81a944738d
Scott Wakefield <scott@expertcc.co.uk>
2017-10-18 13:17:47 +0100

    RA-1276 added retry to framework code repo.

--- src/Esfa.Vacancy.Register.Infrastructure/Repositories/FrameworkCodeRepository.cs
+++ src/Esfa.Vacancy.Register.Infrastructure/Repositories/FrameworkCodeRepository.cs
@@ -4,12 +4,14 @@ using System.Threading.Tasks;
 using Dapper;
 using Esfa.Vacancy.Register.Domain.Repositories;
 using Esfa.Vacancy.Register.Infrastructure.Settings;
+using SFA.DAS.NLog.Logger;
 
 namespace Esfa.Vacancy.Register.Infrastructure.Repositories
 {
     public class FrameworkCodeRepository : IFrameworkCodeRepository
     {
         private readonly IProvideSettings _settings;
+        private readonly ILog _logger;
 
         private const string GetFrameworkCodesQuery = @"
             SELECT 
@@ -19,12 +21,23 @@ namespace Esfa.Vacancy.Register.Infrastructure.Repositories
             WHERE 
                 ApprenticeshipFrameworkStatusTypeId = 1";
 
-        public FrameworkCodeRepository(IProvideSettings settings)
+        public FrameworkCodeRepository(IProvideSettings settings, ILog logger)
         {
             _settings = settings;
+            _logger = logger;
         }
 
         public async Task<IEnumerable<string>> GetAsync()
+        {
+            var retry = VacancyRegisterRetryPolicy.GetFixedIntervalPolicy((exception, time, retryCount, context) =>
+            {
+                _logger.Warn($"Error retrieving framework codes from database: ({exception.Message}). Retrying... attempt {retryCount}");
+            });
+
+            return await retry.ExecuteAsync(InternalGetAsync);
+        }
+
+        private async Task<IEnumerable<string>> InternalGetAsync()
         {
             var connectionString =
                 _settings.GetSetting(ApplicationSettingKeyConstants.AvmsPlusDatabaseConnectionStringKey);
