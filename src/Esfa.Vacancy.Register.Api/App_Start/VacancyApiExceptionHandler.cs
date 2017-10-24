11803cd8abd4a86474fbb637fb9d624e9d389e2f
Chris Jensen <chrisjensenuk@gmail.com>
2017-10-24 10:13:06 +0100

    RA-1278 - Ensuring VacancyApiExceptionHandler never leaks the stack trace

--- src/Esfa.Vacancy.Register.Api/App_Start/VacancyApiExceptionHandler.cs
+++ src/Esfa.Vacancy.Register.Api/App_Start/VacancyApiExceptionHandler.cs
@@ -12,50 +12,57 @@ namespace Esfa.Vacancy.Register.Api.App_Start
     public class VacancyApiExceptionHandler : ExceptionHandler
     {
         private const string GenericErrorMessage = "An internal error occurred, please try again.";
-        
+        private const string ExceptionInExceptionHandlerErrorMessage = "A critical error occurred, please try again.";
+
         public override void Handle(ExceptionHandlerContext context)
         {
-
-            var logger = (ILog)GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(ILog));
-
-            if (context.Exception is ValidationException)
+            try
             {
-                logger.Warn(context.Exception, GetLogMessage("Validation error", context));
-                context.Result = CreateResult(HttpStatusCode.BadRequest, ((ValidationException) context.Exception).Message, context.Request);
-                return;
-            }
+                var logger = (ILog) GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(ILog));
+                
+                if (context.Exception is ValidationException)
+                {
+                    logger.Warn(context.Exception, GetLogMessage("Validation error", context));
+                    context.Result = CreateResult(HttpStatusCode.BadRequest, ((ValidationException) context.Exception).Message, context.Request);
+                    return;
+                }
 
-            if (context.Exception is UnauthorisedException)
-            {
-                logger.Warn(context.Exception, GetLogMessage("Authorisation error", context));
-                context.Result = CreateResult(HttpStatusCode.Unauthorized, ((UnauthorisedException) context.Exception).Message, context.Request);
-                return;
-            }
+                if (context.Exception is UnauthorisedException)
+                {
+                    logger.Warn(context.Exception, GetLogMessage("Authorisation error", context));
+                    context.Result = CreateResult(HttpStatusCode.Unauthorized, ((UnauthorisedException) context.Exception).Message, context.Request);
+                    return;
+                }
 
-            if (context.Exception is ResourceNotFoundException)
-            {
-                logger.Warn(context.Exception, GetLogMessage("Unable to locate resource error", context));
-                context.Result = CreateResult(HttpStatusCode.NotFound, ((ResourceNotFoundException) context.Exception).Message, context.Request);
-                return;
-            }
+                if (context.Exception is ResourceNotFoundException)
+                {
+                    logger.Warn(context.Exception, GetLogMessage("Unable to locate resource error", context));
+                    context.Result = CreateResult(HttpStatusCode.NotFound, ((ResourceNotFoundException) context.Exception).Message, context.Request);
+                    return;
+                }
 
-            if (context.Exception is InfrastructureException)
-            {
-                logger.Error(context.Exception.InnerException, GetLogMessage("Unexpected infrastructure error", context));
+                if (context.Exception is InfrastructureException)
+                {
+                    logger.Error(context.Exception.InnerException, GetLogMessage("Unexpected infrastructure error", context));
+                    context.Result = CreateResult(HttpStatusCode.InternalServerError, GenericErrorMessage, context.Request);
+                    return;
+                }
+
+                logger.Error(context.Exception, GetLogMessage("Unexpected error", context));
                 context.Result = CreateResult(HttpStatusCode.InternalServerError, GenericErrorMessage, context.Request);
-                return;
             }
-
-            logger.Error(context.Exception, GetLogMessage("Unexpected error", context));
-            context.Result = CreateResult(HttpStatusCode.InternalServerError, GenericErrorMessage, context.Request);
-            
+            catch
+            {
+                //whatever happens don't leak the stack trace
+                context.Result = CreateResult(HttpStatusCode.InternalServerError, ExceptionInExceptionHandlerErrorMessage, context.Request);
+            }
         }
 
         private IHttpActionResult CreateResult(HttpStatusCode statusCode, string content, HttpRequestMessage request)
         {
-            var generalResponse = new HttpResponseMessage(statusCode);
-            generalResponse.Content = new StringContent(content);
-            var result = new CustomErrorResult(request, generalResponse);
+            var response = new HttpResponseMessage(statusCode);
+            response.Content = new StringContent(content);
+            var result = new CustomErrorResult(request, response);
             return result;
         }
 
@@ -63,7 +70,7 @@ namespace Esfa.Vacancy.Register.Api.App_Start
         {
             var requestUri = context.Request?.RequestUri?.ToString();
             
-            return !string.IsNullOrEmpty(requestUri) ? $"{message} {requestUri}" : message;
+            return !string.IsNullOrEmpty(requestUri) ? $"{message} url:{requestUri}" : message;
         }
     }
 }
