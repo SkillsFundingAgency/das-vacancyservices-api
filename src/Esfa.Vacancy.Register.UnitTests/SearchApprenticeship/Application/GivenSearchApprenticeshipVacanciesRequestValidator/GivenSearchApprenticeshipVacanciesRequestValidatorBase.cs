db7cfb459472dcc5f7236393ce70773c89583525
Deven Shah <deven.shah@valtech.co.uk>
2017-10-26 15:57:05 +0100

    RA-1266 Rebasing 1 of many

---
+++ src/Esfa.Vacancy.Register.UnitTests/SearchApprenticeship/Application/GivenSearchApprenticeshipVacanciesRequestValidator/GivenSearchApprenticeshipVacanciesRequestValidatorBase.cs
@@ -0,0 +1,24 @@
+﻿using Esfa.Vacancy.Register.Application.Queries.SearchApprenticeshipVacancies;
+using Esfa.Vacancy.Register.Domain.Repositories;
+using Moq;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.Register.UnitTests.SearchApprenticeship.Application.GivenSearchApprenticeshipVacanciesRequestValidator
+{
+    public abstract class GivenSearchApprenticeshipVacanciesRequestValidatorBase
+    {
+        internal Mock<IFrameworkCodeRepository> FrameworkCodeRepositoryMock;
+
+        internal SearchApprenticeshipVacanciesRequestValidator Validator { get; private set; }
+
+        internal static string[] ValidFrameworkCodes = { "1" };
+
+        [SetUp]
+        public void Setup()
+        {
+            FrameworkCodeRepositoryMock = new Mock<IFrameworkCodeRepository>();
+            FrameworkCodeRepositoryMock.Setup(r => r.GetAsync()).ReturnsAsync(ValidFrameworkCodes);
+            Validator = new SearchApprenticeshipVacanciesRequestValidator(FrameworkCodeRepositoryMock.Object);
+        }
+    }
+}
---
+++ src/Esfa.Vacancy.Register.UnitTests/SearchApprenticeship/Application/GivenSearchApprenticeshipVacanciesRequestValidator/WhenValidatingPostedInDays.cs
@@ -0,0 +1,26 @@
+﻿using Esfa.Vacancy.Register.Application.Queries.SearchApprenticeshipVacancies;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.Register.UnitTests.SearchApprenticeship.Application.GivenSearchApprenticeshipVacanciesRequestValidator
+{
+    [TestFixture]
+    public class WhenValidatingPostedInDays : GivenSearchApprenticeshipVacanciesRequestValidatorBase
+    {
+        [TestCase(-1, false, "Posted in days should be 0 or more")]
+        [TestCase(0, true, "Posted in days should be 0 or more")]
+        [TestCase(1, true, "Posted in days should be 0 or more")]
+        public void TestValidation(int postedInDays, bool isValid, string reason)
+        {
+            var request = new SearchApprenticeshipVacanciesRequest()
+            {
+                FrameworkCodes = new[] { "1" },
+                PostedInLastNumberOfDays = postedInDays
+            };
+
+            var result = Validator.Validate(request);
+
+            result.IsValid.Should().Be(isValid, reason);
+        }
+    }
+}
