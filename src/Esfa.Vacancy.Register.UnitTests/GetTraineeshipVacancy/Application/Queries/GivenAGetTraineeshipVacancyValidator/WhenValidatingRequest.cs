62baaebd39f08c90d3cbb990fdee48f1f4ced395
Scott Wakefield <scott@expertcc.co.uk>
2017-10-26 13:31:41 +0100

    RA-1258 modified test names.

--- src/Esfa.Vacancy.Register.UnitTests/GetTraineeshipVacancy/Application/Queries/GivenAGetTraineeshipVacancyValidator/WhenValidatingRequest.cs
+++ src/Esfa.Vacancy.Register.UnitTests/GetTraineeshipVacancy/Application/Queries/GivenAGetTraineeshipVacancyValidator/WhenValidatingRequest.cs
@@ -18,7 +18,7 @@ namespace Esfa.Vacancy.Register.UnitTests.GetTraineeshipVacancy.Application.Quer
 
         [TestCase(0, ErrorCodes.GetTraineeship.VacancyReferenceNumberLessThan0)]
         [TestCase(-1, ErrorCodes.GetTraineeship.VacancyReferenceNumberLessThan0)]
-        public void ThenIfTheVacancyReferenceIsZeroOrLessIsNotValid(int testVacancyReference, string errorCode)
+        public void AndVacancyReferenceIsZeroOrLess_ThenValidationFails(int testVacancyReference, string errorCode)
         {
             _validator
                 .ShouldHaveValidationErrorFor(request => request.Reference, testVacancyReference)
@@ -27,7 +27,7 @@ namespace Esfa.Vacancy.Register.UnitTests.GetTraineeshipVacancy.Application.Quer
 
         [TestCase(1)]
         [TestCase(99999)]
-        public void ThenIfTheVacancyReferenceIsGreaterThanZeroIsValid(int testVacancyReference)
+        public void AndVacancyReferenceIsGreaterThanZero_ThenValidationPasses(int testVacancyReference)
         {
             _validator.ShouldNotHaveValidationErrorFor(request => request.Reference, testVacancyReference);
         }
