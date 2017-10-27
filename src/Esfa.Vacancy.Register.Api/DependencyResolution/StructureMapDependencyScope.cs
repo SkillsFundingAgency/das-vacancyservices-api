2c867071d5ebcbc4b52ee2e3fde7bcfc0f585ced
Deven Shah <deven.shah@valtech.co.uk>
2017-10-27 14:36:42 +0100

    RA-1266 Removed unnecessary ToList
    -Upgraded FluentValidation package for following issue
    https://github.com/JeremySkinner/FluentValidation/issues/527

--- src/Esfa.Vacancy.Register.Api/DependencyResolution/StructureMapDependencyScope.cs
+++ src/Esfa.Vacancy.Register.Api/DependencyResolution/StructureMapDependencyScope.cs
@@ -15,7 +15,8 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace Esfa.Vacancy.Register.Api.DependencyResolution {
+namespace Esfa.Vacancy.Register.Api.DependencyResolution
+{
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -24,11 +25,12 @@ namespace Esfa.Vacancy.Register.Api.DependencyResolution {
     using Microsoft.Practices.ServiceLocation;
 
     using StructureMap;
-	
+
     /// <summary>
     /// The structure map dependency scope.
     /// </summary>
-    public class StructureMapDependencyScope : ServiceLocatorImplBase {
+    public class StructureMapDependencyScope : ServiceLocatorImplBase
+    {
         #region Constants and Fields
 
         private const string NestedContainerKey = "Nested.Container.Key";
@@ -37,8 +39,10 @@ namespace Esfa.Vacancy.Register.Api.DependencyResolution {
 
         #region Constructors and Destructors
 
-        public StructureMapDependencyScope(IContainer container) {
-            if (container == null) {
+        public StructureMapDependencyScope(IContainer container)
+        {
+            if (container == null)
+            {
                 throw new ArgumentNullException("container");
             }
             Container = container;
@@ -50,11 +54,14 @@ namespace Esfa.Vacancy.Register.Api.DependencyResolution {
 
         public IContainer Container { get; set; }
 
-        public IContainer CurrentNestedContainer {
-            get {
+        public IContainer CurrentNestedContainer
+        {
+            get
+            {
                 return (IContainer)HttpContext.Items[NestedContainerKey];
             }
-            set {
+            set
+            {
                 HttpContext.Items[NestedContainerKey] = value;
             }
         }
@@ -63,8 +70,10 @@ namespace Esfa.Vacancy.Register.Api.DependencyResolution {
 
         #region Properties
 
-        private HttpContextBase HttpContext {
-            get {
+        private HttpContextBase HttpContext
+        {
+            get
+            {
                 var ctx = Container.TryGetInstance<HttpContextBase>();
                 return ctx ?? new HttpContextWrapper(System.Web.HttpContext.Current);
             }
@@ -74,26 +83,32 @@ namespace Esfa.Vacancy.Register.Api.DependencyResolution {
 
         #region Public Methods and Operators
 
-        public void CreateNestedContainer() {
-            if (CurrentNestedContainer != null) {
+        public void CreateNestedContainer()
+        {
+            if (CurrentNestedContainer != null)
+            {
                 return;
             }
             CurrentNestedContainer = Container.GetNestedContainer();
         }
 
-        public void Dispose() {
+        public void Dispose()
+        {
             DisposeNestedContainer();
             Container.Dispose();
         }
 
-        public void DisposeNestedContainer() {
-            if (CurrentNestedContainer != null) {
+        public void DisposeNestedContainer()
+        {
+            if (CurrentNestedContainer != null)
+            {
                 CurrentNestedContainer.Dispose();
-				CurrentNestedContainer = null;
+                CurrentNestedContainer = null;
             }
         }
 
-        public IEnumerable<object> GetServices(Type serviceType) {
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
             return DoGetAllInstances(serviceType);
         }
 
@@ -101,14 +116,17 @@ namespace Esfa.Vacancy.Register.Api.DependencyResolution {
 
         #region Methods
 
-        protected override IEnumerable<object> DoGetAllInstances(Type serviceType) {
+        protected override IEnumerable<object> DoGetAllInstances(Type serviceType)
+        {
             return (CurrentNestedContainer ?? Container).GetAllInstances(serviceType).Cast<object>();
         }
 
-        protected override object DoGetInstance(Type serviceType, string key) {
+        protected override object DoGetInstance(Type serviceType, string key)
+        {
             IContainer container = (CurrentNestedContainer ?? Container);
 
-            if (string.IsNullOrEmpty(key)) {
+            if (string.IsNullOrEmpty(key))
+            {
                 return serviceType.IsAbstract || serviceType.IsInterface
                     ? container.TryGetInstance(serviceType)
                     : container.GetInstance(serviceType);
