37937ed7e9a120a70d1cd985278b8765dd084ffb
Deven Shah <deven.shah@valtech.co.uk>
2017-10-31 09:45:29 +0000

    RA-1266 Allow leading and preceeding spaces in codes.
    This bit was lost in refactoring, so adding it back.

---
+++ src/Esfa.Vacancy.Register.UnitTests/SearchApprenticeship/Application/GivenVacancySearchParametersMapper/AndStandardCodes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Esfa.Vacancy.Register.Application.Queries.SearchApprenticeshipVacancies;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.Register.UnitTests.SearchApprenticeship.Application.GivenVacancySearchParametersMapper
+{
+    [TestFixture]
+    public class AndStandardCodes
+    {
+        public static List<TestCaseData> TestCases => new List<TestCaseData>()
+        {
+            new TestCaseData(" 9 , 10 ").SetName("Then any leading and preceding spaces are trimed"),
+            new TestCaseData("").SetName("Then empty list is allowed")
+        };
+
+
+        [TestCaseSource(nameof(TestCases))]
+        public void WhenPopulatingStandardCodes(string standardCodes)
+        {
+            var expectedResult = standardCodes.Split(',').Select(x => x.Trim());
+            var result = VacancySearchParametersMapper.Convert(new SearchApprenticeshipVacanciesRequest()
+            { StandardCodes = standardCodes.Split(',').ToList() });
+
+            result.StandardIds.ShouldAllBeEquivalentTo(expectedResult);
+        }
+    }
+}
