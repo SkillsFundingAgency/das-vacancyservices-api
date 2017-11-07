bcdfd1e66f1c1ebc75470a9e9b5913afe95d6fef
Scott Wakefield <scott@expertcc.co.uk>
2017-11-07 09:07:48 +0000

    RA-1277 refactored tests to ensure both validation errors are returned.

--- src/Esfa.Vacancy.Register.UnitTests/SearchApprenticeship/Application/GivenASearchApprenticeshipVacanciesRequestValidator/WhenValidatingLocationFields.cs
+++ src/Esfa.Vacancy.Register.UnitTests/SearchApprenticeship/Application/GivenASearchApprenticeshipVacanciesRequestValidator/WhenValidatingLocationFields.cs
@@ -5,6 +5,7 @@ using Esfa.Vacancy.Register.Application.Queries.SearchApprenticeshipVacancies;
 using Esfa.Vacancy.Register.Domain.Entities;
 using Esfa.Vacancy.Register.Domain.Validation;
 using FluentAssertions;
+using FluentValidation.Results;
 using NUnit.Framework;
 
 namespace Esfa.Vacancy.Register.UnitTests.SearchApprenticeship.Application.GivenASearchApprenticeshipVacanciesRequestValidator
@@ -29,72 +30,113 @@ namespace Esfa.Vacancy.Register.UnitTests.SearchApprenticeship.Application.Given
         private static List<TestCaseData> FailingTestCases => new List<TestCaseData>
         {
             new TestCaseData(new SearchApprenticeshipVacanciesRequest
+                {
+                    StandardLarsCodes = ValidStandardCodes,
+                    Latitude = 52.399085,
+                    Longitude = -1.506115,
+                    DistanceInMiles = 235
+                }, new ValidationResult())
+                .SetName("And all fields present then valid"),
+            new TestCaseData(new SearchApprenticeshipVacanciesRequest
+                {
+                    StandardLarsCodes = ValidStandardCodes,
+                    Latitude = 52.399085,
+                    Longitude = -1.506115
+                }, new ValidationResult
+                {
+                    Errors = { new ValidationFailure("", "'Distance In Miles' must not be empty.")
                     {
-                        StandardLarsCodes = ValidStandardCodes,
-                        Latitude = 52.399085,
-                        Longitude = -1.506115
-                    },
-                "'Distance In Miles' must not be empty.",
-                ErrorCodes.SearchApprenticeships.DistanceMissingFromGeoSearch)
+                        ErrorCode = ErrorCodes.SearchApprenticeships.DistanceMissingFromGeoSearch
+                    }}
+                })
                 .SetName("And no distance then invalid"),
             new TestCaseData(new SearchApprenticeshipVacanciesRequest
+                {
+                    StandardLarsCodes = ValidStandardCodes,
+                    Longitude = -1.506115,
+                    DistanceInMiles = 342
+                }, new ValidationResult
+                {
+                    Errors = { new ValidationFailure("", "'Latitude' must not be empty.")
                     {
-                        StandardLarsCodes = ValidStandardCodes,
-                        Longitude = -1.506115,
-                        DistanceInMiles = 342
-                    },
-                    "'Latitude' must not be empty.",
-                ErrorCodes.SearchApprenticeships.LatitudeMissingFromGeoSearch)
+                        ErrorCode = ErrorCodes.SearchApprenticeships.LatitudeMissingFromGeoSearch
+                    }}
+                })
                 .SetName("And no latitude then invalid"),
             new TestCaseData(new SearchApprenticeshipVacanciesRequest
+                {
+                    StandardLarsCodes = ValidStandardCodes,
+                    Latitude = 52.399085,
+                    DistanceInMiles = 342
+                }, new ValidationResult
+                {
+                    Errors = { new ValidationFailure("", "'Longitude' must not be empty.")
                     {
-                        StandardLarsCodes = ValidStandardCodes,
-                        Latitude = 52.399085,
-                        DistanceInMiles = 342
-                    },
-                    "'Longitude' must not be empty.",
-                ErrorCodes.SearchApprenticeships.LongitudeMissingFromGeoSearch)
+                        ErrorCode = ErrorCodes.SearchApprenticeships.LongitudeMissingFromGeoSearch
+                    }}
+                })
                 .SetName("And no longitude then invalid"),
             new TestCaseData(new SearchApprenticeshipVacanciesRequest
+                {
+                    StandardLarsCodes = ValidStandardCodes,
+                    Latitude = 52.399085
+                }, new ValidationResult
+                {
+                    Errors = { new ValidationFailure("", "'Longitude' must not be empty.")
                     {
-                        StandardLarsCodes = ValidStandardCodes,
-                        Latitude = 52.399085
-                    },
-                    "'Longitude' must not be empty.",
-                    ErrorCodes.SearchApprenticeships.LongitudeMissingFromGeoSearch)
+                        ErrorCode = ErrorCodes.SearchApprenticeships.LongitudeMissingFromGeoSearch
+                    },new ValidationFailure("", "'Distance In Miles' must not be empty.")
+                    {
+                        ErrorCode = ErrorCodes.SearchApprenticeships.DistanceMissingFromGeoSearch
+                    }}
+                })
                 .SetName("And only latitude then invalid"),
             new TestCaseData(new SearchApprenticeshipVacanciesRequest
+                {
+                    StandardLarsCodes = ValidStandardCodes,
+                    Longitude = -1.506115
+                }, new ValidationResult
+                {
+                    Errors = { new ValidationFailure("", "'Latitude' must not be empty.")
                     {
-                        StandardLarsCodes = ValidStandardCodes,
-                        Longitude = -1.506115
-                    },
-                    "'Latitude' must not be empty.",
-                    ErrorCodes.SearchApprenticeships.LatitudeMissingFromGeoSearch)
+                        ErrorCode = ErrorCodes.SearchApprenticeships.LatitudeMissingFromGeoSearch
+                    },new ValidationFailure("", "'Distance In Miles' must not be empty.")
+                    {
+                        ErrorCode = ErrorCodes.SearchApprenticeships.DistanceMissingFromGeoSearch
+                    }}
+                })
                 .SetName("And only longitude then invalid"),
             new TestCaseData(new SearchApprenticeshipVacanciesRequest
+                {
+                    StandardLarsCodes = ValidStandardCodes,
+                    DistanceInMiles = 342
+                }, new ValidationResult
+                {
+                    Errors = { new ValidationFailure("", "'Latitude' must not be empty.")
                     {
-                        StandardLarsCodes = ValidStandardCodes,
-                        DistanceInMiles = 342
-                    },
-                    "'Latitude' must not be empty.",
-                    ErrorCodes.SearchApprenticeships.LatitudeMissingFromGeoSearch)
+                        ErrorCode = ErrorCodes.SearchApprenticeships.LatitudeMissingFromGeoSearch
+                    },new ValidationFailure("", "'Longitude' must not be empty.")
+                    {
+                        ErrorCode = ErrorCodes.SearchApprenticeships.LongitudeMissingFromGeoSearch
+                    }}
+                })
                 .SetName("And only distance then invalid")
         };
 
         [TestCaseSource(nameof(FailingTestCases))]
-        public void ValidateMissingFields(SearchApprenticeshipVacanciesRequest request, string expectedErrorMessage, string expectedErrorCode)
+        public void ValidateMissingFields(SearchApprenticeshipVacanciesRequest request, ValidationResult expectedResult)
         {
-            var result = Validator.Validate(request);
+            var actualResult = Validator.Validate(request);
 
-            foreach (var validationFailure in result.Errors)
+            foreach (var validationFailure in actualResult.Errors)
             {
                 Console.WriteLine(validationFailure.ErrorMessage);
             }
 
-            result.IsValid.Should().Be(false);
-            
-            result.Errors.First().ErrorMessage.Should().Be(expectedErrorMessage);
-            result.Errors.First().ErrorCode.Should().Be(expectedErrorCode);
+            actualResult.IsValid.Should().Be(expectedResult.IsValid);
+            actualResult.Errors.ShouldAllBeEquivalentTo(expectedResult.Errors,
+                options => options.Including(failure => failure.ErrorMessage)
+                    .Including(failure => failure.ErrorCode));
         }
     }
 }
