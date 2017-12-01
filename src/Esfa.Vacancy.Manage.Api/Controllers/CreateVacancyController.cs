16e43a40e2efd0275039b9332936188c33d8f072
Deven Shah <deven.shah@valtech.co.uk>
2017-12-01 11:49:12 +0000

    RA-1411 Added Manage Api project
    -Enabled swagger

---
+++ src/Esfa.Vacancy.Manage.Api/Controllers/CreateVacancyController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Esfa.Vacancy.Manage.Api.Controllers
+{
+    public class CreateVacancyController : ApiController
+    {
+        public IHttpActionResult Test()
+        {
+            return Ok();
+        }
+    }
+}
