b966c4bf411a3f3c3f2e5fc594918a026d32bc93
Deven Shah <deven.shah@valtech.co.uk>
2017-12-01 15:39:28 +0000

    RA-1411 Added parameter to request mapper

--- src/Esfa.Vacancy.Manage.Api/Orchestrators/CreateApprenticeshipOrchestrator.cs
+++ src/Esfa.Vacancy.Manage.Api/Orchestrators/CreateApprenticeshipOrchestrator.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Tasks;
 using Esfa.Vacancy.Api.Types;
-using Esfa.Vacancy.Application.Commands.CreateApprenticeship;
 using Esfa.Vacancy.Manage.Api.Mappings;
 using MediatR;
 
@@ -8,18 +7,23 @@ namespace Esfa.Vacancy.Manage.Api.Orchestrators
 {
     public class CreateApprenticeshipOrchestrator
     {
+        private readonly ICreateApprenticeshipRequestMapper _createApprenticeshipRequestMapper;
         private readonly IMediator _mediator;
         private readonly ICreateApprenticeshipResponseMapper _apprenticeshipResponseMapper;
 
-        public CreateApprenticeshipOrchestrator(IMediator mediator, ICreateApprenticeshipResponseMapper apprenticeshipResponseMapper)
+        public CreateApprenticeshipOrchestrator(
+            ICreateApprenticeshipRequestMapper createApprenticeshipRequestMapper,
+            IMediator mediator, ICreateApprenticeshipResponseMapper apprenticeshipResponseMapper)
         {
+            _createApprenticeshipRequestMapper = createApprenticeshipRequestMapper;
             _mediator = mediator;
             _apprenticeshipResponseMapper = apprenticeshipResponseMapper;
         }
 
-        public async Task<Vacancy.Api.Types.CreateApprenticeshipResponse> CreateApprecticeship(CreateApprenticeshipParameters parameters)
+        public async Task<CreateApprenticeshipResponse> CreateApprecticeship(CreateApprenticeshipParameters parameters)
         {
-            var response = await _mediator.Send(new CreateApprenticeshipRequest());
+            var request = _createApprenticeshipRequestMapper.MapFromApiParameters(parameters);
+            var response = await _mediator.Send(request);
             return _apprenticeshipResponseMapper.MapToApiResponse(response);
         }
     }
