d31afae2dfe86c75036716c3f3bfcddfcaaddb56
Scott Wakefield <scott@expertcc.co.uk>
2017-12-01 16:57:03 +0000

    RA-1411 added request mapper and tests.

---
+++ src/Esfa.Vacancy.Manage.Api/Mappings/CreateApprenticeshipRequestMapper.cs
@@ -0,0 +1,16 @@
+﻿using Esfa.Vacancy.Api.Types;
+using Esfa.Vacancy.Application.Commands.CreateApprenticeship;
+
+namespace Esfa.Vacancy.Manage.Api.Mappings
+{
+    public class CreateApprenticeshipRequestMapper : ICreateApprenticeshipRequestMapper
+    {
+        public CreateApprenticeshipRequest MapFromApiParameters(CreateApprenticeshipParameters parameters)
+        {
+            return new CreateApprenticeshipRequest
+            {
+                Title = parameters.Title
+            };
+        }
+    }
+}
