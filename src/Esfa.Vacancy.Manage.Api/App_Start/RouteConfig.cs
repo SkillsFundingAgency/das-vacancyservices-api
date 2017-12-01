8217275835df3585e4af6f6af8f63d3628f2a2df
Deven Shah <deven.shah@valtech.co.uk>
2017-12-01 11:49:12 +0000

    RA-1411 Added Manage Api project
    -Enabled swagger

---
+++ src/Esfa.Vacancy.Manage.Api/App_Start/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Esfa.Vacancy.Manage.Api
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.LowercaseUrls = true;
+
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "ApiPage",
+                url: "api",
+                defaults: new { controller = "Home", action = "Api" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/Esfa.Vacancy.Manage.Api/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+
+namespace Esfa.Vacancy.Manage.Api.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController() { }
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("Api", "Home");
+        }
+
+        public ActionResult Api()
+        {
+            return View();
+        }
+
+        //[OutputCache(Duration = 86400)]
+        //public ContentResult RobotsText()
+        //{
+        //    var builder = new StringBuilder();
+
+        //    builder.AppendLine("User-agent: *");
+
+        //    if (!bool.Parse(CloudConfigurationManager.GetSetting("FeatureToggle.RobotsAllowFeature") ?? "false"))
+        //    {
+        //        builder.AppendLine("Disallow: /");
+        //    }
+
+        //    return Content(builder.ToString(), "text/plain", Encoding.UTF8);
+        //}
+    }
+}
