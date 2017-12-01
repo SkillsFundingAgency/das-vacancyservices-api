9601a40e055529e67117a4aa3a50084f9767ab4f
Scott Wakefield <scott@expertcc.co.uk>
2017-12-01 17:10:10 +0000

    RA-1411 added response mapper and tests.

---
+++ src/Esfa.Vacancy.Manage.Api/Mappings/CreateApprenticeshipResponseMapper.cs
@@ -0,0 +1,15 @@
+﻿using Esfa.Vacancy.Application.Commands.CreateApprenticeship;
+
+namespace Esfa.Vacancy.Manage.Api.Mappings
+{
+    public class CreateApprenticeshipResponseMapper : ICreateApprenticeshipResponseMapper
+    {
+        public Vacancy.Api.Types.CreateApprenticeshipResponse MapToApiResponse(CreateApprenticeshipResponse createResponse)
+        {
+            return new Vacancy.Api.Types.CreateApprenticeshipResponse
+            {
+                VacancyReferenceNumber = createResponse.VacancyReferenceNumber
+            };
+        }
+    }
+}
