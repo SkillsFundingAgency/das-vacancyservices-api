788e4f6ca5dd022c10c842318e27809a6b128c20
Deven Shah <deven.shah@valtech.co.uk>
2017-12-01 09:48:49 +0000

    RA-1410 Updated AssemblyInfo with correct titles

--- src/Esfa.Vacancy.UnitTests/Properties/AssemblyInfo.cs
+++ src/Esfa.Vacancy.UnitTests/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Esfa.Vacancy.Register.UnitTests")]
+[assembly: AssemblyTitle("Esfa.Vacancy.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Esfa.Vacancy.Register.UnitTests")]
+[assembly: AssemblyProduct("Esfa.Vacancy.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
