18b335554c8e2f0b8c353059d27c4ce7e8c39498
Scott Wakefield <scott@expertcc.co.uk>
2017-12-01 16:57:03 +0000

    RA-1411 added request mapper and tests.

---
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Api/Mappings/GivenACreateApprenticeshipRequestMapper/WhenMappingTitle.cs
@@ -0,0 +1,24 @@
+﻿using Esfa.Vacancy.Api.Types;
+using Esfa.Vacancy.Manage.Api.Mappings;
+using FluentAssertions;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Api.Mappings.GivenACreateApprenticeshipRequestMapper
+{
+    [TestFixture]
+    public class WhenMappingTitle
+    {
+        [Test]
+        public void ShouldMapTitleToRequest()
+        {
+            var fixture = new Fixture();
+            var parameters = fixture.Create<CreateApprenticeshipParameters>();
+            var mapper = new CreateApprenticeshipRequestMapper();
+
+            var request = mapper.MapFromApiParameters(parameters);
+
+            request.Title.Should().Be(parameters.Title);
+        }
+    }
+}
