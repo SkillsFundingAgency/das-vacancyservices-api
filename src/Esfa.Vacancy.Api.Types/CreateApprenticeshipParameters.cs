25f882fd399d9da1003fcefb2b3e9bfee2a6a34c
Deven Shah <deven.shah@valtech.co.uk>
2017-12-01 12:14:53 +0000

    RA-1411 Created action Parameters and Response objects
    - Renamed Controller to correctly point to Apprenticeship

---
+++ src/Esfa.Vacancy.Api.Types/CreateApprenticeshipParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace Esfa.Vacancy.Api.Types
+{
+    public class CreateApprenticeshipParameters
+    {
+        /// <summary>
+        /// The title of the vacancy.
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
