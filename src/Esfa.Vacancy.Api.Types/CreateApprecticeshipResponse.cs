eabc5eb6ca0c444ffdf50513e9dacf92a98f6bb0
Deven Shah <deven.shah@valtech.co.uk>
2017-12-01 12:14:53 +0000

    RA-1411 Created action Parameters and Response objects
    - Renamed Controller to correctly point to Apprenticeship

---
+++ src/Esfa.Vacancy.Api.Types/CreateApprecticeshipResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Esfa.Vacancy.Api.Types
+{
+    public class CreateApprecticeshipResponse
+    {
+        /// <summary>
+        /// The reference number of the newly created vacancy.
+        /// </summary>
+        public int VacancyReferenceNumber { get; set; }
+    }
+}
---
+++ src/Esfa.Vacancy.Manage.Api/Controllers/CreateApprenticeshipController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Web.Http;
+using Esfa.Vacancy.Api.Types;
+using Swashbuckle.Swagger.Annotations;
+
+namespace Esfa.Vacancy.Manage.Api.Controllers
+{
+    [RoutePrefix("api/v1/apprenticeships")]
+    public class CreateApprenticeshipController : ApiController
+    {
+        /// <summary>
+        /// Creates the specified apprenticeship vacancy.
+        /// </summary>
+        [HttpPost]
+        [AllowAnonymous]
+        [SwaggerOperation("CreateApprenticeshipVacancy", Tags = new[] { "Apprenticeships" })]
+        [SwaggerResponse(HttpStatusCode.OK, "OK", typeof(CreateApprecticeshipResponse))]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "Failed request validation", typeof(BadRequestContent))]
+        public IHttpActionResult Create([FromBody]CreateApprenticeshipParameters createApprenticeshipParameters)
+        {
+            return Ok(new CreateApprecticeshipResponse());
+        }
+    }
+}
