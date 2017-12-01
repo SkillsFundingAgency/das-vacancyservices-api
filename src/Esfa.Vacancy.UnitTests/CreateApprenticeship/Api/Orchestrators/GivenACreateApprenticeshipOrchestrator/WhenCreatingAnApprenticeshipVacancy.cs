2d950b26b439d96b582942c61bcdd6e8b9251bb1
Deven Shah <deven.shah@valtech.co.uk>
2017-12-01 15:39:28 +0000

    RA-1411 Added parameter to request mapper

--- src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Api/Orchestrators/GivenACreateApprenticeshipOrchestrator/WhenCreatingAnApprenticeshipVacancy.cs
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Api/Orchestrators/GivenACreateApprenticeshipOrchestrator/WhenCreatingAnApprenticeshipVacancy.cs
@@ -20,45 +20,61 @@ namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Api.Orchestrators.GivenACr
         private ApiTypes.CreateApprenticeshipResponse _actualResponse;
         private CreateApprenticeshipResponse _expectedMediatorResponse;
         private ApiTypes.CreateApprenticeshipResponse _expectedMapperResponse;
-        private Mock<ICreateApprenticeshipResponseMapper> _mockMapper;
+        private Mock<ICreateApprenticeshipResponseMapper> _mockResponseMapper;
+        private Mock<ICreateApprenticeshipRequestMapper> _mockRequestMapper;
+        private ApiTypes.CreateApprenticeshipParameters _actualParameters;
+        private CreateApprenticeshipRequest _expectedRequest;
 
         [SetUp]
         public async Task SetUp()
         {
             var fixture = new Fixture().Customize(new AutoMoqCustomization());
 
+            _actualParameters = fixture.Create<ApiTypes.CreateApprenticeshipParameters>();
+
             _expectedMediatorResponse = fixture.Create<CreateApprenticeshipResponse>();
             _expectedMapperResponse = fixture.Create<ApiTypes.CreateApprenticeshipResponse>();
+            _expectedRequest = new CreateApprenticeshipRequest();
+
+            _mockRequestMapper = fixture.Freeze<Mock<ICreateApprenticeshipRequestMapper>>(composer => composer.Do(mock => mock
+                .Setup(mapper => mapper.MapFromApiParameters(_actualParameters))
+                .Returns(_expectedRequest)));
 
             _mockMediator = fixture.Freeze<Mock<IMediator>>(composer => composer.Do(mock => mock
                 .Setup(mediator => mediator.Send(It.IsAny<CreateApprenticeshipRequest>(), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(_expectedMediatorResponse)));
 
-            _mockMapper = fixture.Freeze<Mock<ICreateApprenticeshipResponseMapper>>(composer => composer.Do(mock => mock
+            _mockResponseMapper = fixture.Freeze<Mock<ICreateApprenticeshipResponseMapper>>(composer => composer.Do(mock => mock
                 .Setup(mapper => mapper.MapToApiResponse(It.IsAny<CreateApprenticeshipResponse>()))
                 .Returns(_expectedMapperResponse)));
 
             var orchestrator = fixture.Create<CreateApprenticeshipOrchestrator>();
 
-            _actualResponse = await orchestrator.CreateApprecticeship(new ApiTypes.CreateApprenticeshipParameters());
+            _actualResponse = await orchestrator.CreateApprecticeship(_actualParameters);
         }
 
         [Test]
         public void ShouldSendCommandToMediator()
         {
-            _mockMediator.Verify(mediator => mediator.Send(It.IsAny<CreateApprenticeshipRequest>(), It.IsAny<CancellationToken>()), Times.Once);
+            _mockMediator.Verify(mediator => mediator.Send(_expectedRequest, It.IsAny<CancellationToken>()), Times.Once);
         }
 
         [Test]
         public void ShouldInvokeMapperWithMediatorResponse()
         {
-            _mockMapper.Verify(mapper => mapper.MapToApiResponse(_expectedMediatorResponse));
+            _mockResponseMapper.Verify(mapper => mapper.MapToApiResponse(_expectedMediatorResponse));
         }
 
         [Test]
         public void ShouldReturnResponseFromMapper()
         {
             _actualResponse.Should().BeSameAs(_expectedMapperResponse);
+        }
+
+        [Test]
+        public void ShouldInvokeRequestMapperWithInputParameters()
+        {
+            _mockRequestMapper.Verify(mapper => mapper.MapFromApiParameters(_actualParameters));
         }
     }
 }
