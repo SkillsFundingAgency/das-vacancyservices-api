27e5bf5e62755e5b2c64d3479a6065d748ffc14b
Deven Shah <deven.shah@valtech.co.uk>
2017-12-04 12:47:17 +0000

    RA-1411 Added structure map
    - Injected orchestrator in the controller

--- src/Esfa.Vacancy.Manage.Api/Global.asax.cs
+++ src/Esfa.Vacancy.Manage.Api/Global.asax.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Esfa.Vacancy.Manage.Api
@@ -13,11 +8,9 @@ namespace Esfa.Vacancy.Manage.Api
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+            MvcHandler.DisableMvcResponseHeader = true;
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
