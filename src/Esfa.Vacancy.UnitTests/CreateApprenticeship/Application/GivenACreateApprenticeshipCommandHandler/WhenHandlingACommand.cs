c4489ca307f8bbead10d9712713071a0e542e2b0
Scott Wakefield <scott@expertcc.co.uk>
2017-12-06 16:31:52 +0000

    RA-1411 added initial tests for cmd handler for existing functionality.

---
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenACreateApprenticeshipCommandHandler/WhenHandlingACommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Esfa.Vacancy.Application.Commands.CreateApprenticeship;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenACreateApprenticeshipCommandHandler
+{
+    [TestFixture]
+    public class WhenHandlingACommand
+    {
+        [Test]
+        public async Task ThenItShouldReturnATaskWithDefaultRefNumber()
+        {
+            var handler = new CreateApprenticeshipCommandHandler();
+
+            var response = await handler.Handle(new CreateApprenticeshipRequest());
+
+            response.VacancyReferenceNumber.Should().Be(0);
+        }
+    }
+}
