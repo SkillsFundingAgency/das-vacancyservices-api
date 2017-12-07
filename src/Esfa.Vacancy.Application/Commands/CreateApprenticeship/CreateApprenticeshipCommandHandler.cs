f7decf0ff88a48dab8ee4f80b558edc477e79f1d
Scott Wakefield <scott@expertcc.co.uk>
2017-12-07 16:21:17 +0000

    RA-1411 cmd handler uses validator.

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/CreateApprenticeshipCommandHandler.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/CreateApprenticeshipCommandHandler.cs
@@ -1,23 +1,35 @@
 ﻿using System.Threading.Tasks;
 using Esfa.Vacancy.Domain.Entities;
 using Esfa.Vacancy.Domain.Repositories;
+using FluentValidation;
 using MediatR;
 
 namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
 {
     public class CreateApprenticeshipCommandHandler : IAsyncRequestHandler<CreateApprenticeshipRequest, CreateApprenticeshipResponse>
     {
+        private readonly IValidator<CreateApprenticeshipRequest> _validator;
         private readonly IVacancyRepository _vacancyRepository;
 
-        public CreateApprenticeshipCommandHandler(IVacancyRepository vacancyRepository)
+        public CreateApprenticeshipCommandHandler(
+            IValidator<CreateApprenticeshipRequest> validator, 
+            IVacancyRepository vacancyRepository)
         {
+            _validator = validator;
             _vacancyRepository = vacancyRepository;
         }
 
         public async Task<CreateApprenticeshipResponse> Handle(CreateApprenticeshipRequest message)
         {
+            // todo: logging
+            var validationResult = await _validator.ValidateAsync(message);
+
+            if (!validationResult.IsValid)
+                throw new ValidationException(validationResult.Errors);
+
+            // todo: mapping
             var referenceNumber = await _vacancyRepository.CreateApprenticeshipAsync(new CreateApprenticeshipParameters());
-            return await Task.FromResult(new CreateApprenticeshipResponse {VacancyReferenceNumber = referenceNumber});
+            return new CreateApprenticeshipResponse {VacancyReferenceNumber = referenceNumber};
         }
     }
 }
--- src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenACreateApprenticeshipCommandHandler/WhenHandlingACommand.cs
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenACreateApprenticeshipCommandHandler/WhenHandlingACommand.cs
@@ -1,8 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Esfa.Vacancy.Application.Commands.CreateApprenticeship;
 using Esfa.Vacancy.Domain.Entities;
 using Esfa.Vacancy.Domain.Repositories;
 using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
 using Moq;
 using NUnit.Framework;
 using Ploeh.AutoFixture;
@@ -13,24 +18,59 @@ namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenACreateAp
     [TestFixture]
     public class WhenHandlingACommand
     {
-        private Mock<IVacancyRepository> _mockVacancyRepository;
+        private CreateApprenticeshipResponse _createApprenticeshipResponse;
+        private int _expectedRefNumber;
+        private Mock<IValidator<CreateApprenticeshipRequest>> _mockValidator;
+        private IFixture _fixture;
+        private CreateApprenticeshipCommandHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            _expectedRefNumber = _fixture.Create<int>();
+
+            _mockValidator = _fixture.Freeze<Mock<IValidator<CreateApprenticeshipRequest>>>();
+
+            _fixture.Freeze<Mock<IVacancyRepository>>(composer => composer.Do(mock => mock
+                .Setup(repository => repository.CreateApprenticeshipAsync(It.IsAny<CreateApprenticeshipParameters>()))
+                .ReturnsAsync(_expectedRefNumber)));
+
+            _handler = _fixture.Create<CreateApprenticeshipCommandHandler>();
+        }
 
         [Test]
-        public async Task ThenItShouldReturnATaskWithDefaultRefNumber()
+        public void AndCommandNotValid_ThenThrowsValidationException()
         {
-            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            var errorMessage = _fixture.Create<string>();
+            var invalidRequest = _fixture.Create<CreateApprenticeshipRequest>();
 
-            var expectedRefNumber = fixture.Create<int>();
+            _mockValidator
+                .Setup(validator => validator.ValidateAsync(invalidRequest, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ValidationResult(new List<ValidationFailure>
+                {
+                    new ValidationFailure("stuff", errorMessage)
+                }));
 
-            _mockVacancyRepository = fixture.Freeze<Mock<IVacancyRepository>>(composer => composer.Do(mock => mock
-                .Setup(repository => repository.CreateApprenticeshipAsync(It.IsAny<CreateApprenticeshipParameters>()))
-                .ReturnsAsync(expectedRefNumber)));
+            var action = new Func<Task<CreateApprenticeshipResponse>>(() => _handler.Handle(invalidRequest));
 
-            var handler = fixture.Create<CreateApprenticeshipCommandHandler>();
+            action.ShouldThrow<ValidationException>()
+                .WithMessage($"Validation failed: \r\n -- {errorMessage}");
+        }
 
-            var response = await handler.Handle(new CreateApprenticeshipRequest());
+        [Test]
+        public async Task AndCommandValid_ThenReturnsRefNumberFromRepository()
+        {
+            var validRequest = _fixture.Create<CreateApprenticeshipRequest>();
 
-            response.VacancyReferenceNumber.Should().Be(expectedRefNumber);
+            _mockValidator
+                .Setup(validator => validator.ValidateAsync(validRequest, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new ValidationResult());
+
+            _createApprenticeshipResponse = await _handler.Handle(validRequest);
+
+            _createApprenticeshipResponse.VacancyReferenceNumber.Should().Be(_expectedRefNumber);
         }
     }
 }
