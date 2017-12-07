8f7cfc92115e25f50f676f677e8fac8cc90603e9
Scott Wakefield <scott@expertcc.co.uk>
2017-12-07 16:21:17 +0000

    RA-1411 cmd handler uses validator.

--- src/Esfa.Vacancy.Application/Queries/GetApprenticeshipVacancy/GetApprenticeshipVacancyQueryHandler.cs
+++ src/Esfa.Vacancy.Application/Queries/GetApprenticeshipVacancy/GetApprenticeshipVacancyQueryHandler.cs
@@ -11,12 +11,13 @@ namespace Esfa.Vacancy.Application.Queries.GetApprenticeshipVacancy
     public class GetApprenticeshipVacancyQueryHandler : IAsyncRequestHandler<GetApprenticeshipVacancyRequest, GetApprenticeshipVacancyResponse>
     {
         private const string VacancyNotFoundErrorMessage = "The apprenticeship vacancy you are looking for could not be found.";
-        private readonly AbstractValidator<GetApprenticeshipVacancyRequest> _validator;
+        private readonly IValidator<GetApprenticeshipVacancyRequest> _validator;
         private readonly IVacancyRepository _vacancyRepository;
         private readonly ILog _logger;
         private readonly ITrainingDetailService _trainingDetailService;
 
-        public GetApprenticeshipVacancyQueryHandler(AbstractValidator<GetApprenticeshipVacancyRequest> validator,
+        public GetApprenticeshipVacancyQueryHandler(
+            IValidator<GetApprenticeshipVacancyRequest> validator,
             IVacancyRepository vacancyRepository,
             ILog logger,
             ITrainingDetailService trainingDetailService)
