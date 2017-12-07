49966beacade3f1e97f3eac118fd11d81b3c1191
Scott Wakefield <scott@expertcc.co.uk>
2017-12-07 17:29:03 +0000

    RA-1411 added mapper and tests.

---
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/CreateApprenticeshipParametersMapper.cs
@@ -0,0 +1,15 @@
+﻿using Esfa.Vacancy.Domain.Entities;
+
+namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
+{
+    public class CreateApprenticeshipParametersMapper : ICreateApprenticeshipParametersMapper
+    {
+        public CreateApprenticeshipParameters MapFromRequest(CreateApprenticeshipRequest request)
+        {
+            return new CreateApprenticeshipParameters
+            {
+                Title = request.Title
+            };
+        }
+    }
+}
