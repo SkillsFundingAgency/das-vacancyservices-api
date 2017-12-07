a082e8d9eaf8ece7b906f7ead1aa75f4176c2f25
Scott Wakefield <scott@expertcc.co.uk>
2017-12-07 16:38:36 +0000

    RA-1411 logger added to cmd handler.

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/CreateApprenticeshipCommandHandler.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/CreateApprenticeshipCommandHandler.cs
@@ -3,6 +3,7 @@ using Esfa.Vacancy.Domain.Entities;
 using Esfa.Vacancy.Domain.Repositories;
 using FluentValidation;
 using MediatR;
+using SFA.DAS.NLog.Logger;
 
 namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
 {
@@ -10,18 +11,22 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
     {
         private readonly IValidator<CreateApprenticeshipRequest> _validator;
         private readonly IVacancyRepository _vacancyRepository;
+        private readonly ILog _logger;
 
         public CreateApprenticeshipCommandHandler(
             IValidator<CreateApprenticeshipRequest> validator, 
-            IVacancyRepository vacancyRepository)
+            IVacancyRepository vacancyRepository,
+            ILog logger)
         {
             _validator = validator;
             _vacancyRepository = vacancyRepository;
+            _logger = logger;
         }
 
         public async Task<CreateApprenticeshipResponse> Handle(CreateApprenticeshipRequest message)
         {
-            // todo: logging
+            _logger.Info($"Creating new Apprenticeship Vacancy: [{message.Title}]");
+
             var validationResult = await _validator.ValidateAsync(message);
 
             if (!validationResult.IsValid)
@@ -29,6 +34,9 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
 
             // todo: mapping
             var referenceNumber = await _vacancyRepository.CreateApprenticeshipAsync(new CreateApprenticeshipParameters());
+
+            _logger.Info($"Successfully created new Apprenticeship Vacancy: [{message.Title}], Reference Number: [{referenceNumber}]");
+
             return new CreateApprenticeshipResponse {VacancyReferenceNumber = referenceNumber};
         }
     }
