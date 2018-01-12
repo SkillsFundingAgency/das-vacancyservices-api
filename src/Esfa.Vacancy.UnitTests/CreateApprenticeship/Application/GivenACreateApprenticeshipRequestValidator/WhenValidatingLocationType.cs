0ffeda1e00e8e1f1be8d422bbfa43a60e38942f3
Deven Shah <deven.shah@valtech.co.uk>
2018-01-12 17:00:03 +0000

    RA-1513 Added required validation for location type

---
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenACreateApprenticeshipRequestValidator/WhenValidatingLocationType.cs
@@ -0,0 +1,38 @@
+﻿using Esfa.Vacancy.Application.Commands.CreateApprenticeship;
+using Esfa.Vacancy.Domain.Validation;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenACreateApprenticeshipRequestValidator
+{
+    [TestFixture]
+    public class WhenValidatingLocationType
+    {
+        [Test]
+        public void ThenLocationTypeIsRequired()
+        {
+            var request = new CreateApprenticeshipRequest();
+
+            var validator = new CreateApprenticeshipRequestValidator();
+
+            validator
+                .ShouldHaveValidationErrorFor(x => x.LocationType, request)
+                .WithErrorCode(ErrorCodes.CreateApprenticeship.LocationTypeIsRequired);
+        }
+
+        [Test]
+        public void ThenLocationTypeIsValid()
+        {
+            var request = new CreateApprenticeshipRequest()
+            {
+                LocationType = LocationType.OtherLocation
+            };
+
+            var validator = new CreateApprenticeshipRequestValidator();
+
+            validator
+                .ShouldNotHaveValidationErrorFor(x => x.LocationType, request);
+
+        }
+    }
+}
