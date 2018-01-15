d3f552d6da191c8ea18f04b37ddaca468ca2274a
Scott Wakefield <scott@expertcc.co.uk>
2018-01-15 16:50:00 +0000

    RA-1451 moved max length to const.

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/WorkingWeekValidator.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/WorkingWeekValidator.cs
@@ -5,6 +5,7 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
 {
     public partial class CreateApprenticeshipRequestValidator
     {
+        private const int WorkingWeekMaximumLength = 250;
         private const string RegexFreeTextWhitelist = @"^[a-zA-Z0-9\u0080-\uFFA7?$@#()""'!,+\-=_:;.&€£*%\s\/\[\]]+$";
 
         private void WorkingWeekValidator()
@@ -13,7 +14,7 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
                 .NotEmpty()
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.WorkingWeekRequired)
                 .DependentRules(rules => rules.RuleFor(request => request.WorkingWeek)
-                    .MaximumLength(250)
+                    .MaximumLength(WorkingWeekMaximumLength)
                     .WithErrorCode(ErrorCodes.CreateApprenticeship.WorkingWeekLengthGreaterThan250)
                     .Matches(RegexFreeTextWhitelist)
                     .WithErrorCode(ErrorCodes.CreateApprenticeship.WorkingWeekShouldNotIncludeSpecialCharacters)
