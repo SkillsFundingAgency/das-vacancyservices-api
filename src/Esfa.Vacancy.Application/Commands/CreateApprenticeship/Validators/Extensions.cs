cfa7d73d88247a12c7eb64cf6768304bd99faed9
Deven Shah <deven.shah@valtech.co.uk>
2018-01-16 14:51:03 +0000

    RA-1513 Implemented Postcode validation

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/Extensions.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/Extensions.cs
@@ -14,13 +14,23 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
         private const string RegexObjectsBlacklist = @"<\s*o\s*b\s*j\s*e\s*c\s*t\s*[^>]*\s*[^>]*\s*[^>]*>";
 
         public const string WhitelistErrorMessage = "{PropertyName} can't contain invalid characters";
-
         public static IRuleBuilderOptions<T, string> MustContainAllowedFreeTextCharacters<T>(
             this IRuleBuilder<T, string> rule)
         {
             return rule.Matches(RegexFreeTextWhitelist)
                 .WithMessage(WhitelistErrorMessage);
         }
+
+
+        // See http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
+        public const string PostcodeRegex = "^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$";
+        public const string InvalidPostCode = "{PropertyName} is invalid";
+        public static IRuleBuilderOptions<T, string> MustBeAValidPostcode<T>(
+            this IRuleBuilder<T, string> rule)
+        {
+            return rule.Matches(PostcodeRegex).WithMessage(InvalidPostCode);
+        }
+
 
         [Obsolete]
         public static IRuleBuilderOptions<string, string> MatchesAllowedFreeTextCharacters(
