32f194682cf5a31d188f1d5db5b0185154fb0fda
Scott Wakefield <scott@expertcc.co.uk>
2018-01-16 14:39:36 +0000

    RA-1527 moved ExpectedStartDate validator into its own file.

---
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/ExpectedStartDateValidator.cs
@@ -0,0 +1,19 @@
+﻿using Esfa.Vacancy.Domain.Validation;
+using FluentValidation;
+
+namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
+{
+    public partial class CreateApprenticeshipRequestValidator
+    {
+        private void ExpectedStartDateValidator()
+        {
+            RuleFor(request => request.ExpectedStartDate)
+                .NotEmpty()
+                .WithErrorCode(ErrorCodes.CreateApprenticeship.ExpectedStartDateRequired)
+                .DependentRules(rules => rules.RuleFor(request => request.ExpectedStartDate)
+                    .GreaterThanOrEqualTo(request => request.ApplicationClosingDate.Date.AddDays(1))
+                    .WithErrorCode(ErrorCodes.CreateApprenticeship.ExpectedStartDateBeforeClosingDate)
+                    .WithMessage(ErrorMessages.CreateApprenticeship.ExpectedStartDateBeforeClosingDate));
+        }
+    }
+}
