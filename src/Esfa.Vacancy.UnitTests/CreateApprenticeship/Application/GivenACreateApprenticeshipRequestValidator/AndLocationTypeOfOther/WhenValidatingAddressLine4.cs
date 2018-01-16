875e665704f7a5a8f38cd804255670a7be08f746
Deven Shah <deven.shah@valtech.co.uk>
2018-01-16 12:43:18 +0000

    RA-1513 Added validation for AddressLine4

---
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenACreateApprenticeshipRequestValidator/AndLocationTypeOfOther/WhenValidatingAddressLine4.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Esfa.Vacancy.Application.Commands.CreateApprenticeship;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenACreateApprenticeshipRequestValidator.AndLocationTypeOfOther
+{
+    [TestFixture]
+    public class WhenValidatingAddressLine4
+    {
+        private static List<TestCaseData> TestCases() =>
+            new List<TestCaseData>
+            {
+                new TestCaseData(LocationType.OtherLocation, true, null, null).SetName("Then it is not required"),
+                new TestCaseData(LocationType.OtherLocation, false, new string('a', 301),  "31041").SetName("Then cannot exceed 300 characters"),
+                new TestCaseData(LocationType.OtherLocation, false, "<p>", "31042").SetName("Then can include free text white list characters only"),
+                new TestCaseData(LocationType.OtherLocation, true, "10 Downing Street", null).SetName("Then it is valid"),
+            };
+
+        [TestCaseSource(nameof(TestCases))]
+        public void ValidateAddressLine4(LocationType locationType, bool isValid, string addressLine4, string errorCode)
+        {
+            var request = new CreateApprenticeshipRequest()
+            {
+                LocationType = locationType,
+                AddressLine4 = addressLine4
+            };
+
+            var validator = new CreateApprenticeshipRequestValidator();
+
+            if (isValid)
+            {
+                validator.ShouldNotHaveValidationErrorFor(r => r.AddressLine4, request);
+            }
+            else
+            {
+                validator
+                    .ShouldHaveValidationErrorFor(r => r.AddressLine4, request)
+                    .WithErrorCode(errorCode);
+            }
+        }
+    }
+}
