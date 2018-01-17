7ccf5583bd032b7087bec0de1ad4fc27a8c9cce1
Scott Wakefield <scott@expertcc.co.uk>
2018-01-17 16:25:03 +0000

    RA-1527 consolidated the two free text validation methods.

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/Extensions.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/Extensions.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 using Esfa.Vacancy.Domain.Validation;
 using FluentValidation;
-using FluentValidation.Internal;
 
 namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
 {
@@ -12,22 +11,17 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
         private const string RegexScriptsBlacklist = @"<\s*s\s*c\s*r\s*i\s*p\s*t\s*[^>]*\s*[^>]*\s*[^>]*>";
         private const string RegexInputsBlacklist = @"<\s*i\s*n\s*p\s*u\s*t\s*[^>]*\s*[^>]*\s*[^>]*>";
         private const string RegexObjectsBlacklist = @"<\s*o\s*b\s*j\s*e\s*c\s*t\s*[^>]*\s*[^>]*\s*[^>]*>";
-
-        private const string WhitelistErrorMessage = "'{PropertyName}' can't contain invalid characters";
-        public static IRuleBuilderOptions<T, string> MustContainAllowedFreeTextCharacters<T>(
-            this IRuleBuilder<T, string> rule)
-        {
-            return rule.Matches(RegexFreeTextWhitelist)
-                .WithMessage(WhitelistErrorMessage);
-        }
-
         // See http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
-        private const string PostcodeRegex = "^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$";
+        private const string RegexPostcode = "^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$";
+        
+        private const string WhitelistErrorMessage = "'{PropertyName}' can't contain invalid characters";
         private const string InvalidPostCode = "{PropertyName} is invalid";
+
         public static IRuleBuilderOptions<T, string> MustBeAValidPostcode<T>(
             this IRuleBuilder<T, string> rule)
         {
-            return rule.Matches(PostcodeRegex).WithMessage(InvalidPostCode);
+            return rule.Matches(RegexPostcode)
+                .WithMessage(InvalidPostCode);
         }
 
         public static IRuleBuilderOptions<T, string> MatchesAllowedFreeTextCharacters<T>(
--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/LocationValidator.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/LocationValidator.cs
@@ -17,7 +17,7 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine1IsRequired)
                 .MaximumLength(addressLineMaxLength)
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine1MaxLength)
-                .MustContainAllowedFreeTextCharacters()
+                .MatchesAllowedFreeTextCharacters()
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine1ShouldNotIncludeSpecialCharacters);
 
             RuleFor(request => request.AddressLine2)
@@ -26,7 +26,7 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine2IsRequired)
                 .MaximumLength(addressLineMaxLength)
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine2MaxLength)
-                .MustContainAllowedFreeTextCharacters()
+                .MatchesAllowedFreeTextCharacters()
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine2ShouldNotIncludeSpecialCharacters);
 
             RuleFor(request => request.AddressLine3)
@@ -35,20 +35,20 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine3IsRequired)
                 .MaximumLength(addressLineMaxLength)
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine3MaxLength)
-                .MustContainAllowedFreeTextCharacters()
+                .MatchesAllowedFreeTextCharacters()
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine3ShouldNotIncludeSpecialCharacters);
 
             RuleFor(request => request.AddressLine4)
                 .MaximumLength(addressLineMaxLength)
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine4MaxLength)
-                .MustContainAllowedFreeTextCharacters()
+                .MatchesAllowedFreeTextCharacters()
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine4ShouldNotIncludeSpecialCharacters)
                 .When(request => request.LocationType == LocationType.OtherLocation);
 
             RuleFor(request => request.AddressLine5)
                 .MaximumLength(addressLineMaxLength)
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine5MaxLength)
-                .MustContainAllowedFreeTextCharacters()
+                .MatchesAllowedFreeTextCharacters()
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.AddressLine5ShouldNotIncludeSpecialCharacters)
                 .When(request => request.LocationType == LocationType.OtherLocation);
 
@@ -57,7 +57,7 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.TownIsRequired)
                 .MaximumLength(townMaxLength)
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.TownMaxLength)
-                .MustContainAllowedFreeTextCharacters()
+                .MatchesAllowedFreeTextCharacters()
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.TownShouldNotIncludeSpecialCharacters)
                 .When(request => request.LocationType == LocationType.OtherLocation);
 
@@ -67,7 +67,6 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
                 .MustBeAValidPostcode()
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.PostcodeShouldBeValid)
                 .When(request => request.LocationType == LocationType.OtherLocation);
-
         }
     }
 }
