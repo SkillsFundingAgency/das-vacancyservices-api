af18d762d91acfc0d35c6dda80683a2e2d911278
Scott Wakefield <scott@expertcc.co.uk>
2018-01-17 14:23:27 +0000

    RA-1527 removed obsolete functions.

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/Extensions.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/Extensions.cs
@@ -13,7 +13,7 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
         private const string RegexInputsBlacklist = @"<\s*i\s*n\s*p\s*u\s*t\s*[^>]*\s*[^>]*\s*[^>]*>";
         private const string RegexObjectsBlacklist = @"<\s*o\s*b\s*j\s*e\s*c\s*t\s*[^>]*\s*[^>]*\s*[^>]*>";
 
-        public const string WhitelistErrorMessage = "{PropertyName} can't contain invalid characters";
+        private const string WhitelistErrorMessage = "{PropertyName} can't contain invalid characters";
         public static IRuleBuilderOptions<T, string> MustContainAllowedFreeTextCharacters<T>(
             this IRuleBuilder<T, string> rule)
         {
@@ -23,50 +23,24 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
 
 
         // See http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
-        public const string PostcodeRegex = "^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$";
-        public const string InvalidPostCode = "{PropertyName} is invalid";
+        private const string PostcodeRegex = "^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$";
+        private const string InvalidPostCode = "{PropertyName} is invalid";
         public static IRuleBuilderOptions<T, string> MustBeAValidPostcode<T>(
             this IRuleBuilder<T, string> rule)
         {
             return rule.Matches(PostcodeRegex).WithMessage(InvalidPostCode);
         }
 
-
-        [Obsolete]
-        public static IRuleBuilderOptions<string, string> MatchesAllowedFreeTextCharacters(
-            this IRuleBuilder<string, string> rule, string errorCode, string propertyName)
-        {
-            return rule.Matches(RegexFreeTextWhitelist)
-                .WithErrorCode(errorCode)
-                .WithName(propertyName)
-                .WithMessage(string.Format(ErrorMessages.CreateApprenticeship.Whitelist, propertyName));
-        }
-
-        public static IRuleBuilderOptions<CreateApprenticeshipRequest, string> MatchesAllowedFreeTextCharacters(
-            this IRuleBuilder<CreateApprenticeshipRequest, string> rule, string errorCode, string propertyName)
+        public static IRuleBuilderOptions<T, string> MatchesAllowedFreeTextCharacters<T>(
+            this IRuleBuilder<T, string> rule, string errorCode, string propertyName)
         {
             return rule.Matches(RegexFreeTextWhitelist)
                 .WithErrorCode(errorCode)
                 .WithMessage(string.Format(ErrorMessages.CreateApprenticeship.Whitelist, propertyName));
         }
 
-        [Obsolete]
-        public static IRuleBuilderOptions<string, string> MatchesAllowedHtmlFreeTextCharacters(
-            this IRuleBuilder<string, string> rule, string whitelistErrorCode, string blacklistErrorCode, string propertyName)
-        {
-            return rule.Matches(RegexHtmlFreeTextWhitelist)
-                .WithErrorCode(whitelistErrorCode)
-                .WithName(propertyName)
-                .WithMessage(string.Format(ErrorMessages.CreateApprenticeship.Whitelist, propertyName))
-
-                .Must(CheckHtmlFreeTextBlacklist)
-                .WithErrorCode(blacklistErrorCode)
-                .WithName(propertyName)
-                .WithMessage(string.Format(ErrorMessages.CreateApprenticeship.HtmlBlacklist, propertyName));
-        }
-
-        public static IRuleBuilderOptions<CreateApprenticeshipRequest, string> MatchesAllowedHtmlFreeTextCharacters(
-            this IRuleBuilder<CreateApprenticeshipRequest, string> rule, string whitelistErrorCode, string blacklistErrorCode, string propertyName)
+        public static IRuleBuilderOptions<T, string> MatchesAllowedHtmlFreeTextCharacters<T>(
+            this IRuleBuilder<T, string> rule, string whitelistErrorCode, string blacklistErrorCode, string propertyName)
         {
             return rule.Matches(RegexHtmlFreeTextWhitelist)
                 .WithErrorCode(whitelistErrorCode)
