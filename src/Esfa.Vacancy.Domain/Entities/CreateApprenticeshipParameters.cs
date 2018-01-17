10e5c54a47ef1cede1f3f74454cfa729e7f887cd
Deven Shah <deven.shah@valtech.co.uk>
2018-01-17 07:29:08 +0000

    RA-1513 Added location fields to Domain object and respective mapping

--- src/Esfa.Vacancy.Domain/Entities/CreateApprenticeshipParameters.cs
+++ src/Esfa.Vacancy.Domain/Entities/CreateApprenticeshipParameters.cs
@@ -11,5 +11,13 @@ namespace Esfa.Vacancy.Domain.Entities
         public DateTime ExpectedStartDate { get; set; }
         public string WorkingWeek { get; set; }
         public double HoursPerWeek { get; set; }
+        public int LocationType { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string AddressLine4 { get; set; }
+        public string AddressLine5 { get; set; }
+        public string Town { get; set; }
+        public string Postcode { get; set; }
     }
 }
