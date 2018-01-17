2224873da97ef6b45b89c5d767f343b05f53c5bb
Scott Wakefield <scott@expertcc.co.uk>
2018-01-17 15:10:01 +0000

    RA-1527 minor formatting.

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/Extensions.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/Extensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using Esfa.Vacancy.Domain.Validation;
 using FluentValidation;
 using FluentValidation.Internal;
@@ -21,7 +20,6 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
             return rule.Matches(RegexFreeTextWhitelist)
                 .WithMessage(WhitelistErrorMessage);
         }
-
 
         // See http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
         private const string PostcodeRegex = "^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$";
