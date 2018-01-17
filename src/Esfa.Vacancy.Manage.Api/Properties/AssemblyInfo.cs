7fcb6d7bc3e603b3336866619b485a96b43fcd48
sachin.patel <sachin.patel@valtech.co.uk>
2018-01-17 17:07:30 +0000

    RA-1423 :card_index: - Modifying the AssemblyInfo.cs files within the solution to not include assembly version info attributes that will be inserted by GitVersion.

--- src/Esfa.Vacancy.Manage.Api/Properties/AssemblyInfo.cs
+++ src/Esfa.Vacancy.Manage.Api/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Esfa.Vacancy.Manage.Api")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,15 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("79758036-d81e-40be-8e2f-31c2fbcc9565")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
