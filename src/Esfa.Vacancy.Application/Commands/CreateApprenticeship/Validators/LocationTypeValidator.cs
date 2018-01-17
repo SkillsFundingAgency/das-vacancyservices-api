ae8eebdfc63f4c6603ca7669af67153ae68823b8
Scott Wakefield <scott@expertcc.co.uk>
2018-01-17 14:04:53 +0000

    RA-1527 moved LocationTypeValidator to partial class.

---
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/LocationTypeValidator.cs
@@ -0,0 +1,15 @@
+﻿using Esfa.Vacancy.Domain.Validation;
+using FluentValidation;
+
+namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
+{
+    public partial class CreateApprenticeshipRequestValidator
+    {
+        private void ValidateLocationType()
+        {
+            RuleFor(request => request.LocationType)
+                .IsInEnum()
+                .WithErrorCode(ErrorCodes.CreateApprenticeship.LocationTypeIsRequired);
+        }
+    }
+}
