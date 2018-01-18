c97ea0993af8539d361866ada13fdc4ae980fed3
sachin.patel <sachin.patel@valtech.co.uk>
2018-01-18 16:35:46 +0000

    RA-1423 :card_index: - Adding SharedAssemblyInfo.cs to solution.

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Education & Skills Funding Agency")]
+[assembly: AssemblyProduct("Esfa Vacancy API")]
+[assembly: AssemblyCopyright("Copyright ©")]
+[assembly: AssemblyCulture("en-GB")]
