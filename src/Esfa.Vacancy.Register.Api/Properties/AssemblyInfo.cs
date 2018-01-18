00fb3d0a2adcd7377b0ffe1395e38c399263ab97
sachin.patel <sachin.patel@valtech.co.uk>
2018-01-18 16:34:49 +0000

    RA-1423 :card_index: - Modifying the AssemblyInfo.cs files within the solution to not include shared assembly info attributes that will be provided by a linked file.

--- src/Esfa.Vacancy.Register.Api/Properties/AssemblyInfo.cs
+++ src/Esfa.Vacancy.Register.Api/Properties/AssemblyInfo.cs
@@ -6,13 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Esfa.Vacancy.Register.Api")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Esfa.Vacancy.Register.Api")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
