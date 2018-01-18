58b6844aba2056c61b7cdea4d9d3830f86302d51
Scott Wakefield <scott@expertcc.co.uk>
2018-01-18 08:18:55 +0000

    RA-1527 rationalise error message constants.

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/Extensions.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/Extensions.cs
@@ -13,21 +13,19 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
         private const string RegexObjectsBlacklist = @"<\s*o\s*b\s*j\s*e\s*c\s*t\s*[^>]*\s*[^>]*\s*[^>]*>";
         // See http://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
         private const string RegexPostcode = "^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$";
-        
-        private const string InvalidPostCode = "{PropertyName} is invalid";
 
         public static IRuleBuilderOptions<T, string> MustBeAValidPostcode<T>(
             this IRuleBuilder<T, string> rule)
         {
             return rule.Matches(RegexPostcode)
-                .WithMessage(InvalidPostCode);
+                .WithMessage(ErrorMessages.CreateApprenticeship.PostcodeInvalid);
         }
 
         public static IRuleBuilderOptions<T, string> MatchesAllowedFreeTextCharacters<T>(
             this IRuleBuilder<T, string> rule)
         {
             return rule.Matches(RegexFreeTextWhitelist)
-                .WithMessage(ErrorMessages.CreateApprenticeship.Whitelist);
+                .WithMessage(ErrorMessages.CreateApprenticeship.WhitelistFailed);
         }
 
         public static IRuleBuilderOptions<T, string> MatchesAllowedHtmlFreeTextCharacters<T>(
@@ -35,11 +33,11 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
         {
             return rule.Matches(RegexHtmlFreeTextWhitelist)
                 .WithErrorCode(whitelistErrorCode)
-                .WithMessage(ErrorMessages.CreateApprenticeship.Whitelist)
+                .WithMessage(ErrorMessages.CreateApprenticeship.WhitelistFailed)
 
                 .Must(CheckHtmlFreeTextBlacklist)
                 .WithErrorCode(blacklistErrorCode)
-                .WithMessage(ErrorMessages.CreateApprenticeship.HtmlBlacklist);
+                .WithMessage(ErrorMessages.CreateApprenticeship.HtmlBlacklistFailed);
         }
 
         private static bool CheckHtmlFreeTextBlacklist(string text)
