63781368bb9db7c139218e0a3f871edb16d43e6d
Deven Shah <deven.shah@valtech.co.uk>
2018-02-05 15:57:53 +0000

    RA-1419 Mapped employers locationa and correct location type

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/CreateApprenticeshipParametersMapper.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/CreateApprenticeshipParametersMapper.cs
@@ -6,8 +6,8 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
     {
         private readonly IWageTypeMapper _wageTypeMapper;
         private const int StandardLocationType = 1;
-
-        public CreateApprenticeshipParametersMapper(IWageTypeMapper wageTypeMapper)
+	private const int NationwideLocationType = 3;
+        public CreateApprenticeshipParametersMapper(IWageTypeMapper wageTypeMapper)        
         {
             _wageTypeMapper = wageTypeMapper;
         }
@@ -15,7 +15,7 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
         public CreateApprenticeshipParameters MapFromRequest(CreateApprenticeshipRequest request,
             EmployerInformation employerInformation)
         {
-            return new CreateApprenticeshipParameters
+            var parameters = new CreateApprenticeshipParameters
             {
                 Title = request.Title,
                 ShortDescription = request.ShortDescription,
@@ -24,20 +24,42 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
                 ExpectedStartDate = request.ExpectedStartDate,
                 WorkingWeek = request.WorkingWeek,
                 HoursPerWeek = request.HoursPerWeek,
-                WageType = _wageTypeMapper.MapToLegacy(request.WageType),
-                LocationTypeId = StandardLocationType, //This should change when more location type are introduced
-                AddressLine1 = request.AddressLine1,
-                AddressLine2 = request.AddressLine2,
-                AddressLine3 = request.AddressLine3,
-                AddressLine4 = request.AddressLine4,
-                AddressLine5 = request.AddressLine5,
-                Town = request.Town,
-                Postcode = request.Postcode,
+                WageType = (LegacyWageType)request.WageType,
+                LocationTypeId = request.LocationType == LocationType.Nationwide ? NationwideLocationType : StandardLocationType,
                 NumberOfPositions = request.NumberOfPositions,
-                VacancyOwnerRelationshipId = employerInformation.VacancyOwnerRelationshipId.Value,
+                VacancyOwnerRelationshipId = employerInformation.VacancyOwnerRelationshipId.Value, //a value should always exist
                 EmployerDescription = employerInformation.EmployerDescription,
                 EmployersWebsite = employerInformation.EmployerWebsite
             };
+
+            MapLocationFields(request, employerInformation, parameters);
+
+            return parameters;
+        }
+
+        private static void MapLocationFields(CreateApprenticeshipRequest request, EmployerInformation employerInformation,
+            CreateApprenticeshipParameters parameters)
+        {
+            if (request.LocationType == LocationType.OtherLocation)
+            {
+                parameters.AddressLine1 = request.AddressLine1;
+                parameters.AddressLine2 = request.AddressLine2;
+                parameters.AddressLine3 = request.AddressLine3;
+                parameters.AddressLine4 = request.AddressLine4;
+                parameters.AddressLine5 = request.AddressLine5;
+                parameters.Town = request.Town;
+                parameters.Postcode = request.Postcode;
+            }
+            else
+            {
+                parameters.AddressLine1 = employerInformation.AddressLine1;
+                parameters.AddressLine2 = employerInformation.AddressLine2;
+                parameters.AddressLine3 = employerInformation.AddressLine3;
+                parameters.AddressLine4 = employerInformation.AddressLine4;
+                parameters.AddressLine5 = employerInformation.AddressLine5;
+                parameters.Town = employerInformation.Town;
+                parameters.Postcode = employerInformation.Postcode;
+            }
         }
     }
 }
