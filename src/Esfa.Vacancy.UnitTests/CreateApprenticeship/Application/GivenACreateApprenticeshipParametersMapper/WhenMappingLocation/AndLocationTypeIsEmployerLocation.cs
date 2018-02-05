ff361d208f9a4c997de142c6a8df869fbe27d854
Deven Shah <deven.shah@valtech.co.uk>
2018-02-05 15:57:53 +0000

    RA-1419 Mapped employers locationa and correct location type

---
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenACreateApprenticeshipParametersMapper/WhenMappingLocation/AndLocationTypeIsEmployerLocation.cs
@@ -0,0 +1,83 @@
+﻿using Esfa.Vacancy.Application.Commands.CreateApprenticeship;
+using Esfa.Vacancy.Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenACreateApprenticeshipParametersMapper.WhenMappingLocation
+{
+    [TestFixture]
+    public class AndLocationTypeIsEmployerLocation
+    {
+        private CreateApprenticeshipParameters _mappedParameters;
+        private EmployerInformation _employerInformation;
+        private CreateApprenticeshipRequest _createApprenticeshipRequest;
+
+        [SetUp]
+        public void Setup()
+        {
+            var fixture = new Fixture();
+            _createApprenticeshipRequest = fixture.Build<CreateApprenticeshipRequest>()
+                .With(request => request.LocationType, LocationType.EmployerLocation)
+                .Create();
+            _employerInformation = fixture.Create<EmployerInformation>();
+            var mapper = new CreateApprenticeshipParametersMapper();
+            _mappedParameters = mapper.MapFromRequest(_createApprenticeshipRequest, _employerInformation);
+        }
+
+        [Test]
+        public void ThenMapLocationTypeToNationwide()
+        {
+            _mappedParameters.LocationTypeId.Should().Be(1);
+        }
+
+        [Test]
+        public void ThenMapAddressLine1FromEmployeeInformation()
+        {
+            _mappedParameters.AddressLine1.Should().Be(_employerInformation.AddressLine1);
+            _mappedParameters.AddressLine1.Should().NotBe(_createApprenticeshipRequest.AddressLine1);
+        }
+
+        [Test]
+        public void ThenMapAddressLine2FromEmployeeInformation()
+        {
+            _mappedParameters.AddressLine2.Should().Be(_employerInformation.AddressLine2);
+            _mappedParameters.AddressLine2.Should().NotBe(_createApprenticeshipRequest.AddressLine2);
+        }
+
+        [Test]
+        public void ThenMapAddressLine3FromEmployeeInformation()
+        {
+            _mappedParameters.AddressLine3.Should().Be(_employerInformation.AddressLine3);
+            _mappedParameters.AddressLine3.Should().NotBe(_createApprenticeshipRequest.AddressLine3);
+        }
+
+        [Test]
+        public void ThenMapAddressLine4FromEmployeeInformation()
+        {
+            _mappedParameters.AddressLine4.Should().Be(_employerInformation.AddressLine4);
+            _mappedParameters.AddressLine4.Should().NotBe(_createApprenticeshipRequest.AddressLine4);
+        }
+
+        [Test]
+        public void ThenMapAddressLine5FromEmployeeInformation()
+        {
+            _mappedParameters.AddressLine5.Should().Be(_employerInformation.AddressLine5);
+            _mappedParameters.AddressLine5.Should().NotBe(_createApprenticeshipRequest.AddressLine5);
+        }
+
+        [Test]
+        public void ThenMapTownFromEmployeeInformation()
+        {
+            _mappedParameters.Town.Should().Be(_employerInformation.Town);
+            _mappedParameters.Town.Should().NotBe(_createApprenticeshipRequest.Town);
+        }
+
+        [Test]
+        public void ThenMapPostcodeFromEmployeeInformation()
+        {
+            _mappedParameters.Postcode.Should().Be(_employerInformation.Postcode);
+            _mappedParameters.Postcode.Should().NotBe(_createApprenticeshipRequest.Postcode);
+        }
+    }
+}
---
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenACreateApprenticeshipParametersMapper/WhenMappingLocation/AndLocationTypeIsOtherLocation.cs
@@ -0,0 +1,83 @@
+﻿using Esfa.Vacancy.Application.Commands.CreateApprenticeship;
+using Esfa.Vacancy.Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenACreateApprenticeshipParametersMapper.WhenMappingLocation
+{
+    [TestFixture]
+    public class AndLocationTypeIsOtherLocation
+    {
+        private CreateApprenticeshipParameters _mappedParameters;
+        private EmployerInformation _employerInformation;
+        private CreateApprenticeshipRequest _createApprenticeshipRequest;
+
+        [SetUp]
+        public void Setup()
+        {
+            var fixture = new Fixture();
+            _createApprenticeshipRequest = fixture.Build<CreateApprenticeshipRequest>()
+                .With(request => request.LocationType, LocationType.OtherLocation)
+                .Create();
+            _employerInformation = fixture.Create<EmployerInformation>();
+            var mapper = new CreateApprenticeshipParametersMapper();
+            _mappedParameters = mapper.MapFromRequest(_createApprenticeshipRequest, _employerInformation);
+        }
+
+        [Test]
+        public void ThenMapLocationTypeToNationwide()
+        {
+            _mappedParameters.LocationTypeId.Should().Be(1);
+        }
+
+        [Test]
+        public void ThenMapAddressLine1FromEmployeeInformation()
+        {
+            _mappedParameters.AddressLine1.Should().Be(_createApprenticeshipRequest.AddressLine1);
+            _mappedParameters.AddressLine1.Should().NotBe(_employerInformation.AddressLine1);
+        }
+
+        [Test]
+        public void ThenMapAddressLine2FromEmployeeInformation()
+        {
+            _mappedParameters.AddressLine2.Should().Be(_createApprenticeshipRequest.AddressLine2);
+            _mappedParameters.AddressLine2.Should().NotBe(_employerInformation.AddressLine2);
+        }
+
+        [Test]
+        public void ThenMapAddressLine3FromEmployeeInformation()
+        {
+            _mappedParameters.AddressLine3.Should().Be(_createApprenticeshipRequest.AddressLine3);
+            _mappedParameters.AddressLine3.Should().NotBe(_employerInformation.AddressLine3);
+        }
+
+        [Test]
+        public void ThenMapAddressLine5FromEmployeeInformation()
+        {
+            _mappedParameters.AddressLine5.Should().Be(_createApprenticeshipRequest.AddressLine5);
+            _mappedParameters.AddressLine5.Should().NotBe(_employerInformation.AddressLine5);
+        }
+
+        [Test]
+        public void ThenMapTownFromEmployeeInformation()
+        {
+            _mappedParameters.Town.Should().Be(_createApprenticeshipRequest.Town);
+            _mappedParameters.Town.Should().NotBe(_employerInformation.Town);
+        }
+
+        [Test]
+        public void ThenMapPostcodeFromEmployeeInformation()
+        {
+            _mappedParameters.Postcode.Should().Be(_createApprenticeshipRequest.Postcode);
+            _mappedParameters.Postcode.Should().NotBe(_employerInformation.Postcode);
+        }
+
+        [Test]
+        public void ThenMapAddressLine4FromEmployeeInformation()
+        {
+            _mappedParameters.AddressLine4.Should().Be(_createApprenticeshipRequest.AddressLine4);
+            _mappedParameters.AddressLine4.Should().NotBe(_employerInformation.AddressLine4);
+        }
+    }
+}
