ffe6afa768f496cdbd87a71bf49becf8c6c75085
Deven Shah <deven.shah@valtech.co.uk>
2018-02-07 10:01:28 +0000

    RA-1476 Allow empty string to fall through

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/ContactDetailsValidator.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/ContactDetailsValidator.cs
@@ -11,19 +11,20 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
                 .MaximumLength(100)
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.ContactName)
                 .MatchesAllowedFreeTextCharacters()
-                .WithErrorCode(ErrorCodes.CreateApprenticeship.ContactName);
+                .WithErrorCode(ErrorCodes.CreateApprenticeship.ContactName)
+                .When(request => string.IsNullOrWhiteSpace(request.ContactName) == false);
 
             RuleFor(request => request.ContactEmail)
                 .MaximumLength(100)
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.ContactEmail)
                 .MustBeAValidEmailAddress()
-                .WithErrorCode(ErrorCodes.CreateApprenticeship.ContactEmail);
+                .WithErrorCode(ErrorCodes.CreateApprenticeship.ContactEmail)
+                .When(request => string.IsNullOrWhiteSpace(request.ContactEmail) == false);
 
             RuleFor(request => request.ContactNumber)
-                .MaximumLength(16)
-                .WithErrorCode(ErrorCodes.CreateApprenticeship.ContactNumber)
                 .MustBeAValidPhoneNumber()
-                .WithErrorCode(ErrorCodes.CreateApprenticeship.ContactNumber);
+                .WithErrorCode(ErrorCodes.CreateApprenticeship.ContactNumber)
+                .When(request => string.IsNullOrWhiteSpace(request.ContactNumber) == false);
         }
     }
 }
--- src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenACreateApprenticeshipRequestValidator/WhenValidatingContactName.cs
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenACreateApprenticeshipRequestValidator/WhenValidatingContactName.cs
@@ -15,8 +15,13 @@ namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenACreateAp
             new List<TestCaseData>
             {
                 new TestCaseData(null, null, false)
-                    .SetName("And is not provided then is valid."),
-                new TestCaseData(new string('a', 101), "'Contact Name' must be less than 101 characters. You entered 101 characters." , true)
+                    .SetName("And is null then is valid."),
+                new TestCaseData(string.Empty, null, false)
+                    .SetName("And is empty string then is valid."),
+                new TestCaseData(
+                        new string('a', 101),
+                        "'Contact Name' must be less than 101 characters. You entered 101 characters.",
+                        true)
                     .SetName("And if length exceeds 100 then is invalid."),
                 new TestCaseData("<", "'Contact Name' can't contain invalid characters", true)
                 .SetName("And if contains special character then is invalid.")
