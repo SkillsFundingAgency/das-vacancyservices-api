859347311b11adc71af6650502c5327afc418602
Scott Wakefield <scott@expertcc.co.uk>
2018-02-09 15:11:42 +0000

    RA-1420 added service to get wage ranges.

---
+++ src/Esfa.Vacancy.Application/Interfaces/IGetAllApprenticeMinimumWagesService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Esfa.Vacancy.Domain.Entities;
+
+namespace Esfa.Vacancy.Application.Interfaces
+{
+    public interface IGetAllApprenticeMinimumWagesService
+    {
+        Task<IEnumerable<WageRange>> GetAllWagesAsync();
+    }
+}
---
+++ src/Esfa.Vacancy.Domain/Entities/WageRange.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Esfa.Vacancy.Domain.Entities
+{
+    public class WageRange
+    {
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public decimal ApprenticeMinimumWage { get; set; }
+    }
+}
