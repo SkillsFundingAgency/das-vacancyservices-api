caf6ccdfcb5e0f39e331525fe077e475afe72410
Scott Wakefield <scott@expertcc.co.uk>
2018-02-12 10:37:33 +0000

    RA-1420 removed async, added exception scenarios.

--- src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenAMinimumWageCalculator/WhenCallingCalculate.cs
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenAMinimumWageCalculator/WhenCallingCalculate.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using Esfa.Vacancy.Application.Commands.CreateApprenticeship;
 using FluentAssertions;
-using Moq;
 using NUnit.Framework;
-using Ploeh.AutoFixture;
-using Ploeh.AutoFixture.AutoMoq;
 
 namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenAMinimumWageCalculator
 {
@@ -28,12 +24,12 @@ namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenAMinimumW
         };
 
         [TestCaseSource(nameof(TestCases))]
-        public async Task CallCalculate(WageUnit wageUnit, decimal minWage, double hoursPerWeek, decimal expectedResult)
+        public void RunTestCases(WageUnit wageUnit, decimal minWage, double hoursPerWeek, decimal expectedResult)
         {
             var minimumWageCalculator = new MinimumWageCalculator();
 
             var result =
-                await minimumWageCalculator.CalculateMinimumWageAsync(
+                minimumWageCalculator.CalculateMinimumWage(
                     new CreateApprenticeshipRequest
                     {
                         WageUnit = wageUnit,
@@ -43,33 +39,62 @@ namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenAMinimumW
 
             result.Should().Be(expectedResult);
         }
+
+        [Test]
+        public void AndNoMinWage_ThenThrowsException()
+        {
+            var missingMinWageErrorMessage = "MinWage can't be null.";
+            var minimumWageCalculator = new MinimumWageCalculator();
+            Action action = () => minimumWageCalculator.CalculateMinimumWage(
+                new CreateApprenticeshipRequest
+                {
+                    WageUnit = WageUnit.Weekly
+                }); 
+            action.ShouldThrow<ArgumentOutOfRangeException>()
+                .WithMessage($"{missingMinWageErrorMessage}\r\nParameter name: MinWage");
+        }
+
+        [Test]
+        public void AndNotExpectedWageUnit_ThenThrowsException()
+        {
+            var expectedMessage = "WageUnit must be either 'Weekly', 'Monthly' or 'Annually'.";
+            var minimumWageCalculator = new MinimumWageCalculator();
+
+            Action action = () => minimumWageCalculator.CalculateMinimumWage(
+                new CreateApprenticeshipRequest
+                {
+                    MinWage = 300m,
+                    WageType = WageType.Custom
+                });
+
+            action.ShouldThrow<ArgumentOutOfRangeException>()
+                .WithMessage($"{expectedMessage}\r\nParameter name: WageUnit\r\nActual value was 0.");
+        }
     }
 
     public class MinimumWageCalculator
     {
         private const decimal WeeksPerMonth = 4m;
         private const decimal WeeksPerYear = 52m;
+        private const string MissingMinWageErrorMessage = "MinWage can't be null.";
+        private const string IncorrectWageTypeErrorMessage = "WageUnit must be either 'Weekly', 'Monthly' or 'Annually'.";
 
-        public async Task<decimal> CalculateMinimumWageAsync(CreateApprenticeshipRequest request)
+        public decimal CalculateMinimumWage(CreateApprenticeshipRequest request)
         {
-            var actualHourlyWage = 0m;
+            if (!request.MinWage.HasValue)
+                throw new ArgumentOutOfRangeException(nameof(request.MinWage), request.MinWage, MissingMinWageErrorMessage);
 
             switch (request.WageUnit)
             {
                 case WageUnit.Weekly:
-                    actualHourlyWage = decimal.Divide(request.MinWage.Value, (decimal)request.HoursPerWeek);
-                    break;
+                    return decimal.Divide(request.MinWage.Value, (decimal)request.HoursPerWeek);
                 case WageUnit.Monthly:
-                    actualHourlyWage = decimal.Divide(decimal.Divide(request.MinWage.Value, WeeksPerMonth), (decimal)request.HoursPerWeek);
-                    break;
+                    return decimal.Divide(decimal.Divide(request.MinWage.Value, WeeksPerMonth), (decimal)request.HoursPerWeek);
                 case WageUnit.Annually:
-                    actualHourlyWage = decimal.Divide(decimal.Divide(request.MinWage.Value, WeeksPerYear), (decimal)request.HoursPerWeek);
-                    break;
+                    return decimal.Divide(decimal.Divide(request.MinWage.Value, WeeksPerYear), (decimal)request.HoursPerWeek);
                 default:
-                    return decimal.MinValue;
-                    /*throw new ArgumentOutOfRangeException(nameof(request.WageUnit), request.WageUnit, null);*/
+                    throw new ArgumentOutOfRangeException(nameof(request.WageUnit), request.WageUnit, IncorrectWageTypeErrorMessage);
             }
-            return actualHourlyWage;
         }
     }
 }
