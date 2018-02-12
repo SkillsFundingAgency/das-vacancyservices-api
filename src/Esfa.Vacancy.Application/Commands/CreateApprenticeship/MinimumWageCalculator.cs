d896430d92360e895d813ab0b9d1e20a0ee7ccb0
Scott Wakefield <scott@expertcc.co.uk>
2018-02-12 11:08:33 +0000

    RA-1420 moved calculator to proper location.

---
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/MinimumWageCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship
+{
+    public class MinimumWageCalculator
+    {
+        private const decimal WeeksPerMonth = 4m;
+        private const decimal WeeksPerYear = 52m;
+        private const string MissingMinWageErrorMessage = "MinWage can't be null.";
+        private const string IncorrectWageTypeErrorMessage = "WageUnit must be either 'Weekly', 'Monthly' or 'Annually'.";
+
+        public decimal CalculateMinimumWage(CreateApprenticeshipRequest request)
+        {
+            if (!request.MinWage.HasValue)
+                throw new ArgumentOutOfRangeException(nameof(request.MinWage), request.MinWage, MissingMinWageErrorMessage);
+
+            switch (request.WageUnit)
+            {
+                case WageUnit.Weekly:
+                    return decimal.Divide(request.MinWage.Value, (decimal)request.HoursPerWeek);
+                case WageUnit.Monthly:
+                    return decimal.Divide(decimal.Divide(request.MinWage.Value, WeeksPerMonth), (decimal)request.HoursPerWeek);
+                case WageUnit.Annually:
+                    return decimal.Divide(decimal.Divide(request.MinWage.Value, WeeksPerYear), (decimal)request.HoursPerWeek);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(request.WageUnit), request.WageUnit, IncorrectWageTypeErrorMessage);
+            }
+        }
+    }
+}
--- src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenAMinimumWageCalculator/WhenCallingCalculate.cs
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenAMinimumWageCalculator/WhenCallingCalculate.cs
@@ -71,30 +71,4 @@ namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenAMinimumW
                 .WithMessage($"{expectedMessage}\r\nParameter name: WageUnit\r\nActual value was 0.");
         }
     }
-
-    public class MinimumWageCalculator
-    {
-        private const decimal WeeksPerMonth = 4m;
-        private const decimal WeeksPerYear = 52m;
-        private const string MissingMinWageErrorMessage = "MinWage can't be null.";
-        private const string IncorrectWageTypeErrorMessage = "WageUnit must be either 'Weekly', 'Monthly' or 'Annually'.";
-
-        public decimal CalculateMinimumWage(CreateApprenticeshipRequest request)
-        {
-            if (!request.MinWage.HasValue)
-                throw new ArgumentOutOfRangeException(nameof(request.MinWage), request.MinWage, MissingMinWageErrorMessage);
-
-            switch (request.WageUnit)
-            {
-                case WageUnit.Weekly:
-                    return decimal.Divide(request.MinWage.Value, (decimal)request.HoursPerWeek);
-                case WageUnit.Monthly:
-                    return decimal.Divide(decimal.Divide(request.MinWage.Value, WeeksPerMonth), (decimal)request.HoursPerWeek);
-                case WageUnit.Annually:
-                    return decimal.Divide(decimal.Divide(request.MinWage.Value, WeeksPerYear), (decimal)request.HoursPerWeek);
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(request.WageUnit), request.WageUnit, IncorrectWageTypeErrorMessage);
-            }
-        }
-    }
 }
