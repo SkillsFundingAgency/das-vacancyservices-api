48e069c6c2584864ee7cdbb1aac0c8436558e873
Carl Abrahams <carl@charmsolutions.co.uk>
2018-02-12 09:40:54 +0000

    RA 1534 Modified ApplicationMethodValidator

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/ApplicationMethodValidator.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/ApplicationMethodValidator.cs
@@ -8,24 +8,26 @@ namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
         private void ConfigureApplicationMethodValidator()
         {
             RuleFor(request => request.ApplicationMethod)
+                .Cascade(CascadeMode.StopOnFirstFailure)
                 .NotEmpty()
                 .WithErrorCode(ErrorCodes.CreateApprenticeship.ApplicationMethod)
-                .DependentRules(rules => rules.RuleFor(request => request.ApplicationMethod)
-                                              .IsInEnum()
-                                              .WithErrorCode(ErrorCodes.CreateApprenticeship.ApplicationMethod)
-                                              .Must(request => request == ApplicationMethod.Online)
-                                              .WithErrorCode(ErrorCodes.CreateApprenticeship.ApplicationMethod)
-                                              .WithMessage("Invalid Application Method"));
+                .Must(request => request == ApplicationMethod.Online)
+                .WithErrorCode(ErrorCodes.CreateApprenticeship.ApplicationMethod)
+                .WithMessage("Invalid Application Method");
 
             When(request => request.ApplicationMethod == ApplicationMethod.Online, () =>
             {
                 RuleFor(request => request.SupplementaryQuestion1)
-                    .MaximumLength(4000).WithErrorCode(ErrorCodes.CreateApprenticeship.SupplementaryQuestion1)
-                    .MatchesAllowedFreeTextCharacters().WithErrorCode(ErrorCodes.CreateApprenticeship.SupplementaryQuestion1);
+                    .MaximumLength(4000)
+                    .WithErrorCode(ErrorCodes.CreateApprenticeship.SupplementaryQuestion1)
+                    .MatchesAllowedFreeTextCharacters()
+                    .WithErrorCode(ErrorCodes.CreateApprenticeship.SupplementaryQuestion1);
 
                 RuleFor(request => request.SupplementaryQuestion2)
-                    .MaximumLength(4000).WithErrorCode(ErrorCodes.CreateApprenticeship.SupplementaryQuestion2)
-                    .MatchesAllowedFreeTextCharacters().WithErrorCode(ErrorCodes.CreateApprenticeship.SupplementaryQuestion2);
+                    .MaximumLength(4000)
+                    .WithErrorCode(ErrorCodes.CreateApprenticeship.SupplementaryQuestion2)
+                    .MatchesAllowedFreeTextCharacters()
+                    .WithErrorCode(ErrorCodes.CreateApprenticeship.SupplementaryQuestion2);
             });
         }
     }
