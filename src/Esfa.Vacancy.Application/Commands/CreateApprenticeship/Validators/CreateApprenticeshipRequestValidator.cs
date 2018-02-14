cc6f9446a5b0e1a88af1d967b7492963eddb584f
Scott Wakefield <scott@expertcc.co.uk>
2018-02-14 16:05:57 +0000

    RA-1420 reintroduced constructor injection to validator.

--- src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/CreateApprenticeshipRequestValidator.cs
+++ src/Esfa.Vacancy.Application/Commands/CreateApprenticeship/Validators/CreateApprenticeshipRequestValidator.cs
@@ -1,11 +1,20 @@
 ﻿using FluentValidation;
+using SFA.DAS.NLog.Logger;
 
 namespace Esfa.Vacancy.Application.Commands.CreateApprenticeship.Validators
 {
     public partial class CreateApprenticeshipRequestValidator : AbstractValidator<CreateApprenticeshipRequest>
     {
-        public CreateApprenticeshipRequestValidator()
+        private readonly IMinimumWageSelector _minimumWageSelector;
+        private readonly IMinimumWageCalculator _minimumWageCalculator;
+        private readonly ILog _logger;
+
+        public CreateApprenticeshipRequestValidator(IMinimumWageSelector minimumWageSelector, IMinimumWageCalculator minimumWageCalculator, ILog logger)
         {
+            _minimumWageSelector = minimumWageSelector;
+            _minimumWageCalculator = minimumWageCalculator;
+            _logger = logger;
+
             ConfigureTitleValidator();
             ConfigureShortDescriptionValidator();
             ConfigureLongDescriptionValidator();
