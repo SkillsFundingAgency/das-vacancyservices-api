d3d748e08371efeba1722f34cd496a38da880de2
Deven Shah <deven.shah@valtech.co.uk>
2018-02-21 07:56:23 +0000

    RA-1449 Added TrainingDetail domain entity

---
+++ src/Esfa.Vacancy.Domain/Entities/TrainingDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Esfa.Vacancy.Domain.Entities
+{
+    public class TrainingDetail
+    {
+        public int Level { get; set; }
+
+        public DateTime? EffectiveTo { get; set; }
+
+        public bool HasExpired => EffectiveTo > DateTime.Today;
+    }
+}
---
+++ src/Esfa.Vacancy.UnitTests/Shared/Domain/GivenTrainingDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Esfa.Vacancy.Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.UnitTests.Shared.Domain
+{
+    [TestFixture]
+    public class GivenTrainingDetail
+    {
+        [Test]
+        public void AndDateIsBeforeToday_ThenExpiredShouldBeFalse()
+        {
+            var detail = new TrainingDetail { EffectiveTo = DateTime.Today.AddDays(-1) };
+            detail.HasExpired.Should().BeFalse();
+        }
+        [Test]
+        public void AndDateIsAfterToday_ThenExpiredShouldBeTrue()
+        {
+            var detail = new TrainingDetail { EffectiveTo = DateTime.Today.AddDays(1) };
+            detail.HasExpired.Should().BeTrue();
+        }
+        [Test]
+        public void AndDateIsToday_ThenExpiredShouldBeTrue()
+        {
+            var detail = new TrainingDetail { EffectiveTo = DateTime.Today };
+            detail.HasExpired.Should().BeFalse();
+        }
+    }
+}
