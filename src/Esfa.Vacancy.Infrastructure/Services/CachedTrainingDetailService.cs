0fe205ea7f30d2ddf28d23745d932600ac2d88a2
Deven Shah <deven.shah@valtech.co.uk>
2018-02-21 08:06:42 +0000

    RA-1449 Added Cached Training Detail Service.

---
+++ src/Esfa.Vacancy.Infrastructure/Services/CachedTrainingDetailService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Esfa.Vacancy.Application.Interfaces;
+using Esfa.Vacancy.Domain.Entities;
+using Esfa.Vacancy.Domain.Interfaces;
+
+namespace Esfa.Vacancy.Infrastructure.Services
+{
+    public class CachedTrainingDetailService : ITrainingDetailService
+    {
+        private readonly ICacheService _cacheService;
+        private readonly ITrainingDetailService _trainingDetailService;
+
+        public CachedTrainingDetailService(
+            ICacheService cacheService, ITrainingDetailService trainingDetailService)
+        {
+            _cacheService = cacheService;
+            _trainingDetailService = trainingDetailService;
+        }
+
+        public async Task<Framework> GetFrameworkDetailsAsync(int code)
+        {
+            return await _cacheService.CacheAsideAsync<Framework>(
+                $"V1-Framework-{code}",
+                async () => await _trainingDetailService.GetFrameworkDetailsAsync(code),
+                TimeSpan.FromHours(24));
+        }
+
+        public async Task<Standard> GetStandardDetailsAsync(int code)
+        {
+            return await _cacheService.CacheAsideAsync<Standard>(
+                $"V1-Standard-{code}",
+                async () => await _trainingDetailService.GetStandardDetailsAsync(code),
+                TimeSpan.FromHours(24));
+        }
+
+        public async Task<TrainingDetail> GetFrameworkDetailsAsync(string frameworkCode)
+        {
+            return await _cacheService.CacheAsideAsync(
+                $"V2-Framework-{frameworkCode}",
+                async () => await _trainingDetailService.GetFrameworkDetailsAsync(frameworkCode),
+                TimeSpan.FromHours(24));
+        }
+
+        public async Task<TrainingDetail> GetStandardDetailsAsync(string standardLarsCode)
+        {
+            return await _cacheService.CacheAsideAsync(
+                $"V2-Standard-{standardLarsCode}",
+                async () => await _trainingDetailService.GetStandardDetailsAsync(standardLarsCode),
+                TimeSpan.FromHours(12));
+        }
+    }
+}
