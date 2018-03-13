e98ce1f217757bc1d2406ffbfbb9ee08bbab70f8
Scott Wakefield <scott@expertcc.co.uk>
2018-03-13 15:29:50 +0000

    RA-1432 added intitial skeleton of tests for get apprenticeship, tests failing.

---
+++ src/Esfa.Vacancy.AcceptanceTests/Features/GetApprenticeship.feature
@@ -0,0 +1,20 @@
+﻿Feature: GetApprenticeship
+	In order to read an apprenticeship
+	As an api user
+	I want to get the details for an apprenticeship
+
+Scenario: Get an apprenticeship
+	Given the following apprenticeships
+	| Title                  |
+	| Apprentice good things |
+	And I have permission
+	When I get the apprenticeship 'Apprentice good things'
+	Then the http status code should be 200
+
+
+	Scenario: No permission
+	Given the following apprenticeships
+	| Title                  |
+	| Apprentice good things |
+	When I get the apprenticeship 'Apprentice good things'
+	Then the http status code should be 401
---
+++ src/Esfa.Vacancy.AcceptanceTests/Steps/ApprenticeshipSteps.cs
@@ -0,0 +1,43 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Esfa.Vacancy.AcceptanceTests.Steps
+{
+    [Binding]
+    public sealed class ApprenticeshipSteps
+    {
+        [Given("the following apprenticeships")]
+        public void GivenTheFollowingApprenticeships(Table table)
+        {
+            foreach (var row in table.Rows)
+            {
+                var title = row["Title"];
+            }
+
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given("I have permission")]
+        public void GivenIHavePermission()
+        {
+            //todo: setup permission
+
+            ScenarioContext.Current.Pending();
+        }
+
+        [When("I get the apprenticeship (.*)")]
+        public void WhenIGetTheApprenticeship(string apprenticeshipTitle)
+        {
+            //todo: get id from title
+
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then("the http status code should be (.*)")]
+        public void ThenTheResultShouldBe(int httpStatus)
+        {
+            //TODO: implement assert (verification) logic
+
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
