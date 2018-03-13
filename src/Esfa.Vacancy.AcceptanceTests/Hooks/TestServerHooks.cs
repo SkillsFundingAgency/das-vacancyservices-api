0d32863e5ae839db54208cd5f9f66f2c2a3cc582
Scott Wakefield <scott@expertcc.co.uk>
2018-03-13 11:52:46 +0000

    RA-1432 added scaffolding to run tests.

---
+++ src/Esfa.Vacancy.AcceptanceTests/Hooks/TestServerHooks.cs
@@ -0,0 +1,24 @@
+﻿using Esfa.Vacancy.AcceptanceTests.Extensions;
+using Microsoft.Owin.Testing;
+using TechTalk.SpecFlow;
+
+namespace Esfa.Vacancy.AcceptanceTests.Hooks
+{
+    [Binding]
+    public sealed class TestServerHooks
+    {
+        [BeforeFeature]
+        public static void BeforeFeature()
+        {
+            var testServer = TestServer.Create<TestStartUp>();
+            FeatureContext.Current.TestServer(testServer);
+        }
+
+        [AfterFeature]
+        public static void AfterFeature()
+        {
+            var testServer = FeatureContext.Current.TestServer();
+            testServer.Dispose();
+        }
+    }
+}
---
+++ src/Esfa.Vacancy.AcceptanceTests/TestStartUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using Esfa.Vacancy.Register.Api;
+using Esfa.Vacancy.Register.Api.DependencyResolution;
+using Owin;
+using StructureMap;
+
+namespace Esfa.Vacancy.AcceptanceTests
+{
+    public class TestStartUp
+    {
+        internal static IContainer Container { get; private set; }
+
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+
+            Container = IoC.Initialize();
+            config.DependencyResolver = new StructureMapWebApiDependencyResolver(Container);
+
+            WebApiConfig.Register(config);
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
