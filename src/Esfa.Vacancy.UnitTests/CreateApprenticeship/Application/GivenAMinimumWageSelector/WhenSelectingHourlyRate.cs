02aa75f5e4e8d5eba84064228c72434e85022c3c
Chris Jensen <chrisjensenuk@gmail.com>
2019-01-17 13:28:49 +0000

    Fixing unit test that was relying on DateTime.Today that started failing

--- src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenAMinimumWageSelector/WhenSelectingHourlyRate.cs
+++ src/Esfa.Vacancy.UnitTests/CreateApprenticeship/Application/GivenAMinimumWageSelector/WhenSelectingHourlyRate.cs
@@ -16,64 +16,31 @@ namespace Esfa.Vacancy.UnitTests.CreateApprenticeship.Application.GivenAMinimumW
     [TestFixture]
     public class WhenSelectingHourlyRate
     {
-        private static readonly WageRange FirstWageRange = new WageRange
-        {
-            ApprenticeMinimumWage = 3.5m,
-            ValidFrom = DateTime.MinValue,
-            ValidTo = DateTime.Today.AddMonths(-2)
-        };
-
-        private static readonly WageRange SecondWageRange = new WageRange
-        {
-            ApprenticeMinimumWage = 3.7m,
-            ValidFrom = DateTime.Today.AddMonths(-2).AddDays(1).AddHours(12),
-            ValidTo = DateTime.Today.AddMonths(2).AddHours(12)
-        };
-
-        private static readonly WageRange ThirdWageRange = new WageRange
-        {
-            ApprenticeMinimumWage = 3.7m,
-            ValidFrom = DateTime.Today.AddMonths(2).AddDays(1),
-            ValidTo = DateTime.MaxValue
-        };
-
-        private Mock<IGetMinimumWagesService> _mockMinimumWageService;
-        private IGetMinimumWagesService _getMinimumWagesService;
-        private IFixture _fixture;
-
         private static List<TestCaseData> TestCases => new List<TestCaseData>
         {
-            new TestCaseData(DateTime.Today.AddMonths(-15), FirstWageRange.ApprenticeMinimumWage)
-                .SetName("And matches oldest rate Then returns oldest rate"),
-            new TestCaseData(DateTime.Today, SecondWageRange.ApprenticeMinimumWage)
-                .SetName("And matches middle rate Then returns middle rate"),
-            new TestCaseData(DateTime.Today.AddMonths(3), ThirdWageRange.ApprenticeMinimumWage)
-                .SetName("And matches newest rate Then returns newest rate"),
-            new TestCaseData(DateTime.Today.AddMonths(2).AddHours(2), SecondWageRange.ApprenticeMinimumWage)
-                .SetName("And expectedStartDate has time Then strips time to get correct wage range"),
-            new TestCaseData(DateTime.Today.AddMonths(-2).AddDays(1), SecondWageRange.ApprenticeMinimumWage)
-                .SetName("And range.ValidFrom has time Then strips time to get correct wage range"),
-            new TestCaseData(DateTime.Today.AddMonths(2), SecondWageRange.ApprenticeMinimumWage)
-                .SetName("And range.ValidTo has time Then strips time to get correct wage range")
+            new TestCaseData(DateTime.Parse("2016-09-30"), 3.30m )
+                .SetName("And date is pre 1/10/2016"),
+            new TestCaseData(DateTime.Parse("2016-10-01"), 3.40m )
+                .SetName("And date is 1/10/2016"),
+            new TestCaseData(DateTime.Parse("2017-03-20"), 3.40m )
+                .SetName("And date is 20/03/2017"),
+            new TestCaseData(DateTime.Parse("2017-04-01"), 3.50m )
+                .SetName("And date is 1/04/2017"),
+            new TestCaseData(DateTime.Parse("2018-03-03"), 3.50m )
+                .SetName("And date is 30/03/2017"),
+            new TestCaseData(DateTime.Parse("2018-04-01"), 3.70m )
+                .SetName("And date is 01/04/2018"),
+            new TestCaseData(DateTime.Parse("2019-03-30"), 3.70m )
+                .SetName("And date is 30/03/2019"),
+            new TestCaseData(DateTime.Parse("2019-04-01"), 3.90m )
+                .SetName("And date is 01/04/2019"),
         };
-
-        [SetUp]
-        public void SetUp()
-        {
-            _fixture = new Fixture().Customize(new AutoMoqCustomization());
-
-            _mockMinimumWageService = _fixture.Freeze<Mock<IGetMinimumWagesService>>();
-            _mockMinimumWageService
-                .Setup(service => service.GetWageRange(It.IsAny<DateTime>()))
-                .Returns<DateTime>(NationalMinimumWageService.GetHourlyRates);
-
-            _getMinimumWagesService = _fixture.Create<GetMinimumWagesService>();
-        }
 
         [TestCaseSource(nameof(TestCases))]
         public void WhenCallingSelectHourlyRate(DateTime expectedStartDate, decimal expectedHourlyRate)
         {
-            var hourlyRate = _getMinimumWagesService.GetApprenticeMinimumWageRate(expectedStartDate);
+            var getMinimumWagesService = new GetMinimumWagesService();
+            var hourlyRate = getMinimumWagesService.GetApprenticeMinimumWageRate(expectedStartDate);
 
             hourlyRate.Should().Be(expectedHourlyRate);
         }
