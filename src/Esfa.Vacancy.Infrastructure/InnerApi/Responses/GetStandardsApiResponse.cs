699b95288169ce09ee11d516512512d6e3d51a26
Scott Wakefield <scott@expertcc.co.uk>
2021-04-21 16:07:46 +0100

    FAT2-240 adding api response types and cast test for standards, package refs.

---
+++ src/Esfa.Vacancy.Infrastructure/InnerApi/Responses/GetStandardsApiResponse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Esfa.Vacancy.Domain.Entities;
+
+namespace Esfa.Vacancy.Infrastructure.InnerApi.Responses
+{
+    public class GetStandardsApiResponse
+    {
+        public IEnumerable<GetStandardsListItem> Standards { get; set; }
+    }
+
+    public class GetStandardsListItem
+    {
+        public int LarsCode { get; set; }
+        public string Title { get; set; }
+        public int Level { get; set; }
+        public StandardDate StandardDates { get; set; }
+
+        public static explicit operator TrainingDetail(GetStandardsListItem source)
+        {
+            return new TrainingDetail
+            {
+                Title = source.Title,
+                Level = source.Level,
+                EffectiveTo = source.StandardDates?.EffectiveTo,
+                TrainingCode = source.LarsCode.ToString()
+            };
+        }
+    }
+
+    public class StandardDate
+    {
+        public DateTime? LastDateStarts { get; set; }
+
+        public DateTime? EffectiveTo { get; set; }
+
+        public DateTime EffectiveFrom { get; set; }
+    }
+
+    public class GetFrameworksApiResponse
+    {
+        public IEnumerable<GetFrameworksListItem> Frameworks { get; set; }
+    }
+
+    public class GetFrameworksListItem
+    {
+        public string Title { get; set; }
+        public int FrameworkCode { get; set; }
+        public int Level { get; set; }
+        public DateTime? EffectiveTo { get; set; }
+    }
+}
---
+++ src/Esfa.Vacancy.UnitTests/GetApprenticeshipVacancy/Infrastructure/InnerApi/Responses/WhenCastingStandardsListItemToTrainingDetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Esfa.Vacancy.Domain.Entities;
+using Esfa.Vacancy.Infrastructure.InnerApi.Responses;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Esfa.Vacancy.UnitTests.GetApprenticeshipVacancy.Infrastructure.InnerApi.Responses
+{
+    public class WhenCastingStandardsListItemToTrainingDetail
+    {
+        [Test]
+        public void Then_Maps_Values()
+        {
+            var source = new GetStandardsListItem
+            {
+                LarsCode = 23,
+                Level = 3,
+                StandardDates = new StandardDate
+                {
+                    EffectiveTo = DateTime.Today
+                },
+                Title = "Standard title"
+            };
+
+            var result = (TrainingDetail)source;
+
+            result.Title.Should().Be(source.Title);
+            result.Level.Should().Be(source.Level);
+            result.EffectiveTo.Should().Be(source.StandardDates.EffectiveTo);
+            result.TrainingCode.Should().Be(source.LarsCode.ToString());
+        }
+    }
+}
